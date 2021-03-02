a33c6b0b10698314f3e3506897e9c5218ab72c66
jchuahtacc <jchuah@tacc.utexas.edu>
2021-03-02 16:08:33 -0600

    compile via webpack

---
+++ client/webpack.dev.js
@@ -0,0 +1,81 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-cheap-module-source-map',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: 'https://localhost:3000/build/',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js'
+  },
+  // serve assets via webpack serve
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    port: 3000,
+    compress: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /^(.)*\.module\.(css|sass|scss)$/i,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:10]'
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /^((?!\.module).)*\.(css|sass|scss)$/i,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+});
---
+++ client/webpack.prod.js
@@ -0,0 +1,95 @@
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const HtmlWebpackChangeAssetsExtensionPlugin = require('html-webpack-change-assets-extension-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require('path');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/core/static/build/',
+    filename: '[name].[hash].bundle.js',
+    chunkFilename: '[name].[hash].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /^(.)*\.module\.(css|sass|scss)$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:10]'
+              },
+              localsConvention: 'camelCase'
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /^((?!\.module).)*\.(css|sass|scss)$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template:
+        '../server/portal/apps/workbench/templates/portal/apps/workbench/index.j2',
+      filename:
+        '../../server/portal/apps/workbench/templates/portal/apps/workbench/index.html'
+      // jsExtension: '.gz'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].css'
+    }),
+    new CompressionPlugin({
+      // filename: '[path].br[query]',
+      // algorithm: 'brotliCompress',
+      // // test: /\.(js|css|html|svg|png|jpg)$/,
+      // compressionOptions: { level: 11 },
+      // threshold: 10240,
+      // minRatio: 0.8
+    }),
+    new HtmlWebpackChangeAssetsExtensionPlugin()
+  ],
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  }
+});
