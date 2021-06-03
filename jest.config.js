feb3509e5da658c8d92d21af830a2869cf50d21c
jchuahtacc <jchuah@tacc.utexas.edu>
2021-06-03 10:36:01 -0500

    Translate tsx through babel-jest

--- jest.config.js
+++ jest.config.js
@@ -160,8 +160,7 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '\\.(js|jsx)?$': 'babel-jest',
-    '\\.(ts|tsx)$': 'ts-jest'
+    '\\.(js|jsx|ts|tsx)?$': 'babel-jest'
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
