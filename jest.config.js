9dc0694e941d8ba568bc982e885477c0f27dc04b
jchuahtacc <jchuah@tacc.utexas.edu>
2021-06-03 10:37:33 -0500

    lint

--- jest.config.js
+++ jest.config.js
@@ -160,7 +160,7 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '\\.(js|jsx|ts|tsx)?$': 'babel-jest'
+    '\\.(js|jsx|ts|tsx)?$': 'babel-jest',
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
