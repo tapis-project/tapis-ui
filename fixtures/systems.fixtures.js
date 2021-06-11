0ddde75fa8938ed2e870d623bb07adce46112016
jchuahtacc <jchuah@tacc.utexas.edu>
2021-06-11 11:19:48 -0500

    linting

--- fixtures/systems.fixtures.js
+++ fixtures/systems.fixtures.js
@@ -1,19 +1,19 @@
 export const tapisSystem = {
-  "id": "testuser8-e2e",
-  "systemType": "LINUX",
-  "owner": "testuser8",
-  "host": "129.114.17.47",
-  "effectiveUserId": "testuser2",
-  "defaultAuthnMethod": "PKI_KEYS",
-  "canExec": false
-}
+  id: 'testuser8-e2e',
+  systemType: 'LINUX',
+  owner: 'testuser8',
+  host: '129.114.17.47',
+  effectiveUserId: 'testuser2',
+  defaultAuthnMethod: 'PKI_KEYS',
+  canExec: false,
+};
 
 export const systemsStore = {
   systems: {
-    results: [ tapisSystem ],
+    results: [tapisSystem],
     offset: 0,
     limit: -1,
     loading: false,
-    error: null
-  }
+    error: null,
+  },
 };
