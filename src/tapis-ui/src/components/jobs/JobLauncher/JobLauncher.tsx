37a5b668308d78f7f62d4e685f068528b5c7cc46
jchuahtacc <jchuah@tacc.utexas.edu>
2021-07-02 13:51:54 -0500

    Form Field Wrapper typescript class

--- src/tapis-ui/src/components/jobs/JobLauncher/JobLauncher.tsx
+++ src/tapis-ui/src/components/jobs/JobLauncher/JobLauncher.tsx
@@ -3,7 +3,6 @@ import { useSystems } from 'tapis-redux';
 import { Config } from 'tapis-redux/types';
 import { Jobs } from '@tapis/tapis-typescript';
 import { DescriptionList } from 'tapis-ui/_common';
-import { Button, FormGroup } from 'reactstrap';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import JobSubmit from 'tapis-ui/components/jobs/JobSubmit';
@@ -14,10 +13,76 @@ import {
   SectionMessage
 } from 'tapis-ui/_common';
 import JobFormField from './JobFormField';
+import JobFieldWrapper, { JobFieldWrapperProps } from './JobFieldWrapper';
 import * as Yup from 'yup';
+import {
+  Button,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Badge,
+  InputGroup,
+  InputGroupAddon,
+} from 'reactstrap';
+
+
+type JobFieldType = {
+  name: string,
+  type: string,
+  required: boolean, 
+  description: string,
+  label: string,
+  addon?: React.ReactNode,
+  addonType?: 'prepend' | 'append'
+}
+
+const jobFields: Array<JobFieldWrapperProps> = [
+  {
+    props: {
+      name: 'name',
+      type: 'string'
+    },
+    description: 'A name for this job',
+    label: 'Name',
+    required: true,
+    children: <Input bsSize="sm" />
+  },
+  {
+    props: {
+      name: 'appId',
+      type: 'string',  
+    },
+    description: 'The ID of the TAPIS application to run',
+    label:'App ID',
+    required: true,
+    children: <Input bsSize="sm" />
+  },
+  {
+    props: {
+      name: 'appVersion',
+      type: 'string',
+    },
+    description: 'The version of the application to run',
+    label: 'App Version',
+    required: true,
+    children: <Input bsSize="sm" />
+  },
+  {
+    props: {
+      name: 'execSystemId',
+      type: 'string',
+    },
+    description: 'A TAPIS system that can run this application',
+    label: 'Execution System',
+    required: true,
+    children: <Input bsSize="sm" />
+  }
+]
+
+
 
 export type OnSubmitCallback = (job: Jobs.Job) => any;
-
 interface JobLauncherProps {
   config?: Config,
   initialValues?: Jobs.ReqSubmitJob,
@@ -41,6 +106,7 @@ const JobLauncherProps: React.FC<JobLauncherProps> = ({ config, initialValues, o
   )
 
   const validate = (values) => {
+    console.log("Validate", values);
   }
 
   const formSubmit = (values, { setSubmitting }) => {
@@ -57,20 +123,19 @@ const JobLauncherProps: React.FC<JobLauncherProps> = ({ config, initialValues, o
       >
        {({ isSubmitting }) => (
          <Form>
-           <JobFormField 
-            addon={undefined} addonType={''} 
-            description="Job Name" 
-            label="Job Name"
-            required={true}
-            type="string" 
-            name="name" />
-           <ErrorMessage name="name" component="div" />
-           <Field type="string" name="appId" />
-           <ErrorMessage name="appId" component="div" />
-           <Field type="string" name="appVersion" />
-           <ErrorMessage name="appVersion" component="div" />
-           <Field type="string" name="execSystemId" />
-           <ErrorMessage name="execSystemId" component="div" />
+           {
+             jobFields.map(field => {
+               return (
+                 <JobFieldWrapper 
+                   props={field.props}
+                   label={field.label}
+                   required={field.required}
+                   children={field.children}
+                   description={field.description}
+                 />
+               )
+             })
+           }
            <button type="submit">Submit</button>
          </Form>
        )}
