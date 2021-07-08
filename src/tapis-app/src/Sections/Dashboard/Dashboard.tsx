21247566557afb3e80d6e18fd84d19bcb80b5512
jchuahtacc <jchuah@tacc.utexas.edu>
2021-07-08 14:38:00 -0500

    Dashboard shows stats

--- src/tapis-app/src/Sections/Dashboard/Dashboard.tsx
+++ src/tapis-app/src/Sections/Dashboard/Dashboard.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { SectionHeader, Icon } from 'tapis-ui/_common';
 import { Card, CardHeader, CardBody, CardTitle, CardFooter, CardText } from 'reactstrap';
+import { useAuthenticator, useSystems, useApps, useJobs  } from 'tapis-redux';
+import { LoadingSpinner } from 'tapis-ui/_common';
 import './Dashboard.module.scss';
 import './Dashboard.scss';
 
@@ -10,16 +13,17 @@ type DashboardCardProps = {
   link: string,
   counter: string,
   name: string,
-  text: string
+  text: string,
+  loading: boolean
 }
 
-const DashboardCard: React.FC<DashboardCardProps> = ({icon, link, counter, name, text}) => {
+const DashboardCard: React.FC<DashboardCardProps> = ({icon, link, counter, name, text, loading}) => {
   return (
     <Card styleName="card">
       <CardHeader>
         <div styleName="card-header">
           <div>
-            <Icon name="data-files" className="dashboard__card-icon"/>
+            <Icon name={icon} className="dashboard__card-icon"/>
           </div>          
           <div>
             {name}
@@ -27,7 +31,13 @@ const DashboardCard: React.FC<DashboardCardProps> = ({icon, link, counter, name,
         </div>
       </CardHeader>
       <CardBody>
-        <CardTitle tag="h5">{counter}</CardTitle>
+        <CardTitle tag="h5">
+          {
+            loading 
+              ? <LoadingSpinner placement="inline" />
+              : <div>{counter}</div>
+          }
+        </CardTitle>
         <CardText>
           {text}
         </CardText>
@@ -41,31 +51,81 @@ const DashboardCard: React.FC<DashboardCardProps> = ({icon, link, counter, name,
 }
 
 const Dashboard: React.FC = () => {
+  const { token } = useAuthenticator();
+  const systems = useSystems();
+  const jobs = useJobs();
+  const apps = useApps();
+  const dispatch = useDispatch();
+
+  useEffect(
+    () => {
+      if (token) {
+        dispatch(systems.list({}));
+        dispatch(jobs.list({}));
+        dispatch(apps.list({}));
+      }
+    },
+    [ token ]
+  )
+
+
   return (
     <div>
       <SectionHeader className="dashboard__section-header">Dashboard for {process.env.TAPIS_TENANT_URL}</SectionHeader>
       <div styleName="cards">
-        <DashboardCard
-          icon="data-files"
-          name="Systems"
-          text="Access TAPIS systems and files"
-          link="/systems"
-          counter=""
-        />
-        <DashboardCard
-          icon="applications"
-          name="Applications"
-          text="View TAPIS applications and launch jobs"
-          link="/apps"
-          counter=""
-        />
-        <DashboardCard
-          icon="jobs"
-          name="Jobs"
-          text="View status and details for previously launched TAPIS jobs"
-          link="/jobs"
-          counter=""
-        />
+        {
+          token ? (
+            <>
+              <DashboardCard
+                icon="data-files"
+                name="Systems"
+                text="Access TAPIS systems and files"
+                link="/systems"
+                counter={`${systems.systems.results.length} systems`}
+                loading={systems.systems.loading}
+              />
+              <DashboardCard
+                icon="applications"
+                name="Applications"
+                text="View TAPIS applications and launch jobs"
+                link="/apps"
+                counter={`${apps.apps.results.length} apps`}
+                loading={apps.apps.loading}
+              />
+              <DashboardCard
+                icon="jobs"
+                name="Jobs"
+                text="View status and details for previously launched TAPIS jobs"
+                link="/jobs"
+                counter={`${jobs.jobs.results.length} jobs`}
+                loading={jobs.jobs.loading}
+              />
+            </>
+          ) : (
+            <Card>
+              <CardHeader>
+                <div styleName="card-header">
+                  <div>
+                    <Icon name="user" className="dashboard__card-icon"/>
+                  </div>          
+                  <div>
+                    You are not logged in
+                  </div>
+                </div>
+              </CardHeader>
+              <CardBody>
+                <CardTitle>
+                  Please log in to use TAPIS
+                </CardTitle>
+              </CardBody>
+              <CardFooter styleName="card-footer">
+                <Link to="/login">Proceed to login</Link>
+                <Icon name="push-right" /> 
+              </CardFooter>
+            </Card>
+          )
+        }
+
       </div>
     </div>
   ) 
