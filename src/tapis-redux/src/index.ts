5baa4fcc229428effb053fe6d1bed38dc793af49
jchuahtacc <jchuah@tacc.utexas.edu>
2021-07-12 09:35:25 -0500

    isExpired utils

--- src/tapis-redux/src/index.ts
+++ src/tapis-redux/src/index.ts
@@ -3,5 +3,6 @@ export { default as useSystems } from './systems/useSystems';
 export { default as useFiles } from './files/useFiles';
 export { default as useApps } from './apps/useApps';
 export { default as useJobs } from './jobs/useJobs';
+export * as utils from './utils';
 export * as store from './store';
 
