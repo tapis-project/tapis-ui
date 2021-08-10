73b60137585a1b4c47dcfb20940e31f8343fab63
jchuahtacc <jchuah@tacc.utexas.edu>
2021-08-10 13:18:01 -0500

    Add tapis-provider to component testing utils

--- src/tapis-ui/__tests__/utils/testing.js
+++ src/tapis-ui/__tests__/utils/testing.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { BrowserRouter, Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import TapisProvider from 'tapis-hooks/provider';
 
 export default function renderComponent(component, store, history) {
   if (history) {
     return render(
-      <Provider store={store}>
-        <Router history={history}>{component}</Router>
-      </Provider>
+      <TapisProvider tenantUrl="https://tacc.tapis.io">
+        <Provider store={store}>
+          <Router history={history}>{component}</Router>
+        </Provider>
+      </TapisProvider>
     );
   }
   return render(
-    <Provider store={store}>
-      <BrowserRouter>{component}</BrowserRouter>
-    </Provider>
+    <TapisProvider tenantUrl="https://tacc.tapis.io"> 
+      <Provider store={store}>
+        <BrowserRouter>{component}</BrowserRouter>
+      </Provider>
+    </TapisProvider>
   );
 }
