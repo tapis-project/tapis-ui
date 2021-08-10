932e1219e6c37006043bde40123022aba0140194
jchuahtacc <jchuah@tacc.utexas.edu>
2021-08-10 13:51:09 -0500

    Cache token in cookies

--- src/tapis-hooks/src/provider/TapisProvider.tsx
+++ src/tapis-hooks/src/provider/TapisProvider.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import {
-  useQuery,
-  useMutation,
-  useQueryClient,
   QueryClient,
   QueryClientProvider,
 } from 'react-query'
+import Cookies from 'js-cookie';
 import { Authenticator } from '@tapis/tapis-typescript';
 import TapisContext, { TapisContextType } from '../context/TapisContext';
 
@@ -16,14 +14,47 @@ interface TapisProviderProps {
 }
 
 const TapisProvider: React.FC<TapisProviderProps> = ({ token, tenantUrl, children }) => {
+
   const [ accessToken, setAccessToken ] = useState<Authenticator.NewAccessTokenResponse>(token);
+  const accessTokenCallback = useCallback(
+    (token) => {
+      setAccessToken(token);
+      Cookies.set('tapis-token', JSON.stringify(token));
+    },
+    [setAccessToken]
+  )
   const contextValue: TapisContextType  = {
     accessToken,
-    setAccessToken,
+    setAccessToken: accessTokenCallback,
     tenantUrl
   }
 
+  // Attempt to load cached cookie
+  useEffect(
+    () => {
+      // A passed token will override anything in Cookies
+      if (token) {
+        return
+      };
+
+      // Attempt to load token from cookie
+      const cookie = Cookies.get('tapis-token');
+      if (!cookie) return;
+      const cookieToken: Authenticator.NewAccessTokenResponse = JSON.parse(cookie);
+
+      // Do not load expired tokens
+      const expiry = Date.parse(cookieToken.expires_at);
+      if (expiry < Date.now()) {
+        return;
+      }
+
+      setAccessToken(cookieToken);
+    },
+    [token, setAccessToken]
+  )
+
   const queryClient = new QueryClient();
+
 
   return (
     <TapisContext.Provider value={contextValue}>
