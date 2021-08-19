c09277a737c8b914f8fd975fc686f86ff29eae1e
jchuahtacc <jchuah@tacc.utexas.edu>
2021-08-19 16:07:05 -0500

    task/TUI-104 -- apps list react query (#119)
    * apps list in tapis-hooks and AppsListing
    
    * Fix unit tests
    
    * Dashboard uses apps listing
    
    * Add query keys
    
    * Code quality changes

--- src/tapis-ui/components/apps/AppsListing/AppsListing.test.tsx
+++ src/tapis-ui/components/apps/AppsListing/AppsListing.test.tsx
@@ -3,13 +3,24 @@ import configureStore from 'redux-mock-store';
 import '@testing-library/jest-dom/extend-expect';
 import renderComponent from 'utils/testing';
 import { AppsListing } from 'tapis-ui/components/apps';
+import { useList } from 'tapis-hooks/apps';
+import { tapisApp } from 'fixtures/apps.fixtures';
 import tapisReduxStore from 'fixtures/tapis-redux.fixture';
 
 const mockStore = configureStore();
 
+jest.mock('tapis-hooks/apps');
+
 describe('Apps', () => {
   it('renders AppsListing component', () => {
     const store = mockStore(tapisReduxStore);
+    (useList as jest.Mock).mockReturnValue({
+      data: {
+        result: [ tapisApp ],
+      },
+      isLoading: false,
+      error: null
+    })
 
     const { getAllByText } = renderComponent(<AppsListing />, store);
     expect(getAllByText(/SleepSeconds/).length).toEqual(1);
