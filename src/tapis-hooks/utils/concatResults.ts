563a728f5e268d2668547644c51635c3c9845f14
jchuahtacc <jchuah@tacc.utexas.edu>
2021-08-19 17:20:06 -0500

    Simplified concatResults function

--- src/tapis-hooks/utils/concatResults.ts
+++ src/tapis-hooks/utils/concatResults.ts
@@ -2,13 +2,11 @@ export interface ResultType<T> {
   result: T[]
 }
 
-export const concatResults = <T extends unknown>(pages: ResultType<T>[]): T[] => {
-  const reducedPages = pages.reduce(
-    (accumulator, current) => {
-      return {
-        result: accumulator?.result?.concat(current?.result ?? [])
-      };
-    }
-  )
-  return reducedPages.result ?? [];
-}
+export const concatResults = <T extends unknown>(
+  pages: ResultType<T>[]
+): T[] => {
+  const reducedPages = pages.reduce((accumulator, current) => {
+    return accumulator.concat(current?.result ?? [])
+  }, [] as T[]);
+  return reducedPages
+};
