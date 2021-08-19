e0e23d7c332f4358e19a685aeae07244dd251f8f
jchuahtacc <jchuah@tacc.utexas.edu>
2021-08-19 16:05:20 -0500

    Code quality changes

--- src/tapis-api/apps/list.ts
+++ src/tapis-api/apps/list.ts
@@ -1,6 +1,5 @@
 import { Apps } from '@tapis/tapis-typescript';
-import { apiGenerator } from 'tapis-api/utils';
-import { errorDecoder } from 'tapis-api/utils';
+import { apiGenerator, errorDecoder } from 'tapis-api/utils';
 
 const list = (params: Apps.GetAppsRequest, basePath: string, jwt: string) => {
   const api: Apps.ApplicationsApi = apiGenerator<Apps.ApplicationsApi>(Apps, Apps.ApplicationsApi, basePath, jwt);
