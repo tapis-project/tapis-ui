70185b2c8a6e009c5fdb863fac5e657f1b60fc3d
jchuahtacc <jchuah@tacc.utexas.edu>
2021-08-20 10:00:18 -0500

    Add useSubmit hook

---
+++ src/tapis-hooks/jobs/useSubmit.ts
@@ -0,0 +1,44 @@
+import { useMutation } from 'react-query';
+import { Jobs } from '@tapis/tapis-typescript';
+import { submit } from 'tapis-api/jobs';
+import { useTapisConfig } from 'tapis-hooks';
+
+type SubmitHookParams = {
+  request: Jobs.ReqSubmitJob,
+  onSuccess?: (data: Jobs.RespSubmitJob) => any,
+  onError?: (error: any) => any
+}
+
+const useSubmit = () => {
+  const { basePath, accessToken } = useTapisConfig();
+  const jwt = accessToken?.access_token || '';
+
+  // The useMutation react-query hook is used to call operations that make server-side changes
+  // (Other hooks would be used for data retrieval)
+  //
+  // In this case, loginHelper is called to perform the operation, with an onSuccess callback
+  // passed as an option
+  const { mutate, isLoading, isError, isSuccess, error } = useMutation(submit);
+
+  // Return hook object with loading states and login function
+  return {
+    isLoading,
+    isError,
+    isSuccess,
+    error,
+    submit: (params: SubmitHookParams) => {
+      const { request, onSuccess, onError } = params;
+
+      // Call mutate to trigger a single post-like API operation
+      return mutate(
+        { request, basePath, jwt },
+        { 
+          onSuccess,
+          onError
+        }
+      )
+    }
+  }
+}
+
+export default useSubmit;
