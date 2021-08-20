3c4b3a7f84e613d0b056bc3347699ceeb3614647
jchuahtacc <jchuah@tacc.utexas.edu>
2021-08-20 12:16:35 -0500

    Query keys for systems

--- src/tapis-hooks/systems/useList.ts
+++ src/tapis-hooks/systems/useList.ts
@@ -2,11 +2,12 @@ import { useQuery } from 'react-query';
 import { list } from 'tapis-api/systems';
 import { Systems } from '@tapis/tapis-typescript'
 import { useTapisConfig } from 'tapis-hooks';
+import QueryKeys from './queryKeys';
 
 const useList = (params: Systems.GetSystemsRequest) => {
   const { accessToken, basePath } = useTapisConfig();
   const result = useQuery<Systems.RespSystems, Error>(
-    [params, accessToken],
+    [QueryKeys.list, params, accessToken],
     // Default to no token. This will generate a 403 when calling the list function
     // which is expected behavior for not having a token
     () => list(params, basePath, accessToken?.access_token || ''),
