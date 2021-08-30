e27f21da0763df270989a1ff3a8e4f7925984259
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2021-08-30 16:28:29 -0500

    abstract sidebar nav into a single component (#136)
    * abstract sidebar nav into a single component
    
    * move navbar to tapis-ui

--- src/tapis-ui/components/apps/AppsListing/AppsListing.tsx
+++ src/tapis-ui/components/apps/AppsListing/AppsListing.tsx
@@ -1,61 +1,46 @@
 import React from 'react';
-import { NavLink, useRouteMatch } from 'react-router-dom'
+import { useRouteMatch } from 'react-router-dom';
 import { useList } from 'tapis-hooks/apps';
-import { LoadingSpinner, Message, Icon } from 'tapis-ui/_common';
+import { LoadingSpinner, Message } from 'tapis-ui/_common';
 import { Apps } from '@tapis/tapis-typescript';
-import './AppsListing.scss';
+import { Navbar, NavItem } from 'tapis-ui/components/Navbar';
 
-interface AppsListingItemProps {
-  app: Apps.TapisApp,
-}
-
-const AppsListingItem: React.FC<AppsListingItemProps> = ({ app }) => {
+const AppsListing: React.FC = () => {
+  const { data, isLoading, error } = useList();
   const { url } = useRouteMatch();
-  return (
-    <li className="nav-item">
-      <NavLink to={`${url}/${app.id}/${app.version}`} className={"nav-link"} activeClassName={"active"}>
-        <div className="nav-content">
-          <Icon name="applications" /> {/* we'll want to set name based on the app */}
-          <span className="nav-text">{`${app.id} v${app.version}`}</span>
-        </div>
-      </NavLink>
-    </li>
-  );
-};
-
-interface AppsListingProps {
-  className?: string,
-}
-
-const AppsListing: React.FC<AppsListingProps> = ({ className }) => {
-
-  const { data, isLoading, error } = useList()
 
   if (isLoading) {
-    return <LoadingSpinner />
+    return <LoadingSpinner />;
   }
 
   if (error) {
-    return <Message canDismiss={false} type="error" scope="inline">{(error as any).message}</Message>
+    return (
+      <Message canDismiss={false} type="error" scope="inline">
+        {(error as any).message}
+      </Message>
+    );
   }
 
   const appList: Array<Apps.TapisApp> = data?.result || [];
 
   return (
-    <div className={className ? className : "apps-list nav flex-column"}>
-      { 
-        appList.length
-          ? appList.map((app: Apps.TapisApp | null) => {
-              return app && (
-                <AppsListingItem
-                  app={app}
-                  key={app.id}
-                />
-              )
-            })
-          : <i>No applications found</i>
-      }
-    </div>
+    <Navbar>
+      {appList.length ? (
+        appList.map((app: Apps.TapisApp | null) => {
+          return (
+            app && (
+              <NavItem
+                to={`${url}/${app.id}/${app.version}`}
+                icon="applications"
+                key={app.id}
+              >{`${app.id} v${app.version}`}</NavItem>
+            )
+          );
+        })
+      ) : (
+        <i>No applications found</i>
+      )}
+    </Navbar>
   );
 };
 
