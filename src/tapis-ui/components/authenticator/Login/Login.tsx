0fffe715ab8c65762e6ae0fa76be50d31ff1192b
Jarosenb <jrosenberg@tacc.utexas.edu>
2021-09-01 14:58:45 -0500

    replace Formik with react-hook-form

--- src/tapis-ui/components/authenticator/Login/Login.tsx
+++ src/tapis-ui/components/authenticator/Login/Login.tsx
@@ -2,79 +2,71 @@ import React from 'react';
 import { Input, Button } from 'reactstrap';
 import { useLogin } from 'tapis-hooks/authenticator';
 import { useTapisConfig } from 'tapis-hooks/context';
-import { Formik, Form } from 'formik';
+import { useForm } from 'react-hook-form';
 import { FieldWrapper } from 'tapis-ui/_common';
 import { SubmitWrapper } from 'tapis-ui/_wrappers';
-import * as Yup from 'yup';
 
 const Login: React.FC = () => {
   const { login, isLoading, error } = useLogin();
   const { accessToken } = useTapisConfig();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: { username: null, password: null } });
 
-  const validationSchema = (props: any) => {
-    return Yup.lazy((values: any) => {
-      const schema = Yup.object({
-        username: Yup.string().required().min(1),
-        password: Yup.string().required().min(1),
-      });
-      return schema;
-    });
-  };
+  const { ref: usernameRef, ...usernameFieldProps } = register('username', {
+    required: 'Username is a required field',
+  });
+  const { ref: passwordRef, ...passwordFieldProps } = register('password', {
+    required: 'Password is a required field',
+  });
 
-  const formSubmit = (
-    values: any,
-    { setSubmitting }: { setSubmitting: any }
-  ) => {
-    const { username, password } = values;
-    login(username, password);
-    setSubmitting(false);
-  };
-
-  const initialValues = {
-    username: '',
-    password: '',
-  };
+  const onSubmit = ({
+    username,
+    password,
+  }: {
+    username: string;
+    password: string;
+  }) => login(username, password);
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={formSubmit}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <FieldWrapper
-            label="Username"
-            required={true}
-            description="Your TAPIS username"
-            props={{ name: 'username', type: 'string' }}
-          >
-            <Input bsSize="sm" />
-          </FieldWrapper>
-          <FieldWrapper
-            label="Password"
-            required={true}
-            description="Your TAPIS password"
-            props={{ name: 'password', type: 'password' }}
-          >
-            <Input bsSize="sm" />
-          </FieldWrapper>
-          <SubmitWrapper
-            isLoading={isLoading}
-            error={error}
-            success={accessToken && 'Successfully logged in'}
-          >
-            <Button
-              type="submit"
-              className="btn btn-primary"
-              disabled={isSubmitting || isLoading || accessToken != null}
-            >
-              Log In
-            </Button>
-          </SubmitWrapper>
-        </Form>
-      )}
-    </Formik>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FieldWrapper
+        label="Username"
+        required={true}
+        description="Your TAPIS username"
+        error={errors['username']}
+      >
+        <Input bsSize="sm" {...usernameFieldProps} innerRef={usernameRef} />
+      </FieldWrapper>
+      <FieldWrapper
+        label="Password"
+        required={true}
+        description="Your TAPIS password"
+        error={errors['password']}
+      >
+        <Input
+          bsSize="sm"
+          {...passwordFieldProps}
+          innerRef={passwordRef}
+          type="password"
+        />
+      </FieldWrapper>
+      <SubmitWrapper
+        isLoading={isLoading}
+        error={error}
+        success={accessToken && 'Successfully logged in'}
+      >
+        <Button
+          type="submit"
+          className="btn btn-primary"
+          disabled={isLoading || accessToken != null}
+        >
+          Log In
+        </Button>
+      </SubmitWrapper>
+    </form>
   );
 };
 
