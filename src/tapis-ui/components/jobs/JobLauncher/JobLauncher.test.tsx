c5b2f260eb61c3d8e60ebd2811159416578dcb73
Jarosenb <jrosenberg@tacc.utexas.edu>
2021-09-01 10:15:10 -0500

    add prettier check to the linter

--- src/tapis-ui/components/jobs/JobLauncher/JobLauncher.test.tsx
+++ src/tapis-ui/components/jobs/JobLauncher/JobLauncher.test.tsx
@@ -2,18 +2,9 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import renderComponent from 'utils/testing';
 import { JobLauncher } from 'tapis-ui/components/jobs';
-import { Jobs } from '@tapis/tapis-typescript';
 
 describe('JobLauncher', () => {
   it('renders JobsListing component', () => {
-    const appId = "SleepSeconds";
-    const appVersion = "0.0.1";
-    const initialValues: Jobs.ReqSubmitJob = {
-      appId,
-      appVersion,
-      name: `Mock Job`,
-      execSystemId: 'tapisv3-exec'
-    }
     const { getAllByTestId } = renderComponent(
       <JobLauncher appId="SleepSeconds" appVersion="0.1" name="test-job" execSystemId="exec-system "/>
     );
