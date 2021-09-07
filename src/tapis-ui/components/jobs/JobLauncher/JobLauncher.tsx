6297862861390f67db0c9f913669278702dded13
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2021-09-07 12:46:18 -0500

    Prettier

--- src/tapis-ui/components/jobs/JobLauncher/JobLauncher.tsx
+++ src/tapis-ui/components/jobs/JobLauncher/JobLauncher.tsx
@@ -47,32 +47,68 @@ const JobLauncher: React.FC<JobLauncherProps> = ({
   }, [setValue, name, appId, appVersion, execSystemId]);
 
   return (
-    <QueryWrapper isLoading={systemsListHook.isLoading} error={systemsListHook.error}>
+    <QueryWrapper
+      isLoading={systemsListHook.isLoading}
+      error={systemsListHook.error}
+    >
       <form onSubmit={handleSubmit(formSubmit)}>
         {/* Required fields */}
-        <FieldWrapper description='A name for this job' label='Name' 
-          required={true} error={errors['name']}
+        <FieldWrapper
+          description="A name for this job"
+          label="Name"
+          required={true}
+          error={errors['name']}
         >
-          <Input bsSize="sm" defaultValue={name} 
-            {...mapInnerRef(register('name', { required: 'Name is required'}))} />
+          <Input
+            bsSize="sm"
+            defaultValue={name}
+            {...mapInnerRef(register('name', { required: 'Name is required' }))}
+          />
         </FieldWrapper>
-        <FieldWrapper description='The ID of the TAPIS application to run' label='App ID' 
-          required={true} error={errors['appId']}
+        <FieldWrapper
+          description="The ID of the TAPIS application to run"
+          label="App ID"
+          required={true}
+          error={errors['appId']}
         >
-          <Input bsSize="sm" data-testid="appId" defaultValue={appId} 
-            {...mapInnerRef(register('appId', { required: 'App ID is required'}))} />
+          <Input
+            bsSize="sm"
+            data-testid="appId"
+            defaultValue={appId}
+            {...mapInnerRef(
+              register('appId', { required: 'App ID is required' })
+            )}
+          />
         </FieldWrapper>
-        <FieldWrapper description='The version of the application to run' label='App Version'
-          required={true}  error={errors['appVersion']}
+        <FieldWrapper
+          description="The version of the application to run"
+          label="App Version"
+          required={true}
+          error={errors['appVersion']}
         >
-          <Input bsSize="sm" defaultValue={appVersion}
-            {...mapInnerRef(register('appVersion', { required: 'App version is required '}))} /> 
+          <Input
+            bsSize="sm"
+            defaultValue={appVersion}
+            {...mapInnerRef(
+              register('appVersion', { required: 'App version is required ' })
+            )}
+          />
         </FieldWrapper>
-        <FieldWrapper description='A TAPIS system that can run this application' label='Execution System'
-          required={true} error={errors['execSystemId']}
+        <FieldWrapper
+          description="A TAPIS system that can run this application"
+          label="Execution System"
+          required={true}
+          error={errors['execSystemId']}
         >
-          <Input type="select" defaultValue={execSystemId}
-            {...mapInnerRef(register('execSystemId', { required: 'An execution system is required '}))} >
+          <Input
+            type="select"
+            defaultValue={execSystemId}
+            {...mapInnerRef(
+              register('execSystemId', {
+                required: 'An execution system is required ',
+              })
+            )}
+          >
             {systems.map((system: TapisSystem) => (
               <option key={system.id}>{system.id}</option>
             ))}
@@ -80,10 +116,18 @@ const JobLauncher: React.FC<JobLauncherProps> = ({
         </FieldWrapper>
 
         {/* Submit button */}
-        <SubmitWrapper error={error} isLoading={isLoading} 
-          success={data && `Successfully submitted job ${data?.result?.uuid ?? ''}` }
+        <SubmitWrapper
+          error={error}
+          isLoading={isLoading}
+          success={
+            data && `Successfully submitted job ${data?.result?.uuid ?? ''}`
+          }
         >
-          <Button type="submit" className="btn btn-primary" disabled={isLoading || !!error}>
+          <Button
+            type="submit"
+            className="btn btn-primary"
+            disabled={isLoading || !!error}
+          >
             Submit Job
           </Button>
         </SubmitWrapper>
