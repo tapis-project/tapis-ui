1739cf81f2c4c5c95d1542a5a478145e9ffd2e41
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2021-09-07 12:21:30 -0500

    Use QueryWrapper for launcher form

--- src/tapis-ui/components/jobs/JobLauncher/JobLauncher.tsx
+++ src/tapis-ui/components/jobs/JobLauncher/JobLauncher.tsx
@@ -5,7 +5,7 @@ import { useForm, UseFormRegisterReturn } from 'react-hook-form';
 import FieldWrapper, { FieldWrapperProps } from 'tapis-ui/_common/FieldWrapper';
 import { TapisSystem } from '@tapis/tapis-typescript-systems';
 import { Jobs } from '@tapis/tapis-typescript';
-import { SubmitWrapper } from 'tapis-ui/_wrappers';
+import { SubmitWrapper, QueryWrapper } from 'tapis-ui/_wrappers';
 import { Button, Input } from 'reactstrap';
 
 export type OnSubmitCallback = (job: Jobs.Job) => any;
@@ -29,7 +29,7 @@ const JobLauncher: React.FC<JobLauncherProps> = ({
   name,
   execSystemId,
 }) => {
-  const systemsListHook = useList({});
+  const systemsListHook = useList();
   const systems: Array<TapisSystem> = systemsListHook.data?.result ?? [];
 
   const { submit, isLoading, error, data } = useSubmit(appId, appVersion);
@@ -52,54 +52,48 @@ const JobLauncher: React.FC<JobLauncherProps> = ({
   }, [setValue, name, appId, appVersion, execSystemId]);
 
   return (
-    <div>
-      {!!systems.length && (
-        <form onSubmit={handleSubmit(formSubmit)}>
-          {/* Required fields */}
-          <FieldWrapper description='A name for this job' label='Name' 
-            required={true} error={errors['name']}>
-            <Input bsSize="sm" defaultValue={name} 
-              {...mapInnerRef(register('name', { required: 'Name is required'}))} />
-          </FieldWrapper>
-          <FieldWrapper description='The ID of the TAPIS application to run' label='App ID' 
-            required={true} error={errors['appId']}>
-            <Input bsSize="sm" data-testid="appId" defaultValue={appId} 
-              {...mapInnerRef(register('appId', { required: 'App ID is required'}))} />
-          </FieldWrapper>
-          <FieldWrapper description='The version of the application to run' label='App Version'
-            required={true}  error={errors['appVersion']}>
-            <Input bsSize="sm" defaultValue={appVersion}
-              {...mapInnerRef(register('appVersion', { required: 'App version is required '}))} /> 
-          </FieldWrapper>
-          <FieldWrapper description='A TAPIS system that can run this application' label='Execution System'
-            required={true} error={errors['execSystemId']}>
-            <Input type="select" defaultValue={execSystemId}
-              {...mapInnerRef(register('execSystemId', { required: 'An execution system is required '}))} >
-              {systems.map((system: TapisSystem) => (
-                <option key={system.id}>{system.id}</option>
-              ))}
-            </Input>
-          </FieldWrapper>
+    <QueryWrapper isLoading={systemsListHook.isLoading} error={systemsListHook.error}>
+      <form onSubmit={handleSubmit(formSubmit)}>
+        {/* Required fields */}
+        <FieldWrapper description='A name for this job' label='Name' 
+          required={true} error={errors['name']}
+        >
+          <Input bsSize="sm" defaultValue={name} 
+            {...mapInnerRef(register('name', { required: 'Name is required'}))} />
+        </FieldWrapper>
+        <FieldWrapper description='The ID of the TAPIS application to run' label='App ID' 
+          required={true} error={errors['appId']}
+        >
+          <Input bsSize="sm" data-testid="appId" defaultValue={appId} 
+            {...mapInnerRef(register('appId', { required: 'App ID is required'}))} />
+        </FieldWrapper>
+        <FieldWrapper description='The version of the application to run' label='App Version'
+          required={true}  error={errors['appVersion']}
+        >
+          <Input bsSize="sm" defaultValue={appVersion}
+            {...mapInnerRef(register('appVersion', { required: 'App version is required '}))} /> 
+        </FieldWrapper>
+        <FieldWrapper description='A TAPIS system that can run this application' label='Execution System'
+          required={true} error={errors['execSystemId']}
+        >
+          <Input type="select" defaultValue={execSystemId}
+            {...mapInnerRef(register('execSystemId', { required: 'An execution system is required '}))} >
+            {systems.map((system: TapisSystem) => (
+              <option key={system.id}>{system.id}</option>
+            ))}
+          </Input>
+        </FieldWrapper>
 
-          {/* Submit button */}
-          <SubmitWrapper
-            error={error}
-            isLoading={isLoading}
-            success={
-              data && `Successfully submitted job ${data?.result?.uuid ?? ''}`
-            }
-          >
-            <Button
-              type="submit"
-              className="btn btn-primary"
-              disabled={isLoading || !!error}
-            >
-              Submit Job
-            </Button>
-          </SubmitWrapper>
-        </form>
-      )}
-    </div>
+        {/* Submit button */}
+        <SubmitWrapper error={error} isLoading={isLoading} 
+          success={data && `Successfully submitted job ${data?.result?.uuid ?? ''}` }
+        >
+          <Button type="submit" className="btn btn-primary" disabled={isLoading || !!error}>
+            Submit Job
+          </Button>
+        </SubmitWrapper>
+      </form>
+    </QueryWrapper>
   );
 };
 
