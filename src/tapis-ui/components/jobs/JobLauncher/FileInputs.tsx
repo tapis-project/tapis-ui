2d7d2d46bf1e45ea460914a0c523962800b62948
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2021-09-14 14:55:36 -0500

    Expand DictField into explicit File Input wrapped inputs

--- src/tapis-ui/components/jobs/JobLauncher/FileInputs.tsx
+++ src/tapis-ui/components/jobs/JobLauncher/FileInputs.tsx
@@ -1,38 +1,69 @@
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 import { FileInput } from '@tapis/tapis-typescript-apps';
 import { FieldArray, FieldArrayComponent } from './FieldArray';
-import { DictField, FieldSpec } from './DictField';
-import { Button } from 'reactstrap';
+import FieldWrapper from 'tapis-ui/_common/FieldWrapper';
+import { Button, Input, Label, FormText, FormGroup } from 'reactstrap';
+import { mapInnerRef } from 'tapis-ui/utils/forms';
 import styles from './FileInputs.module.scss';
 
 const FileInputField: FieldArrayComponent = ({ refName, item, remove }) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
   const { sourceUrl, targetPath, inPlace, id } = item;
-  const fieldSpecs: Array<FieldSpec> = [
-    {
-      name: 'sourceUrl',
-      label: 'Source URL',
-      required: 'Source URL is required',
-      description: 'Input TAPIS file as a pathname, TAPIS URI or web URL',
-      defaultValue: sourceUrl,
-    },
-    {
-      name: 'targetPath',
-      label: 'Target Path',
-      required: 'Target is required',
-      description: 'File mount path inside of running container',
-      defaultValue: targetPath,
-    },
-    {
-      name: 'inPlace',
-      label: 'In Place',
-      description:
-        "If this is true, the source URL will be mounted from the execution system's local file system",
-      defaultChecked: inPlace,
-    },
-  ];
+
   return (
     <div className={styles.input} key={id}>
-      <DictField refName={refName} fieldSpecs={fieldSpecs} />
+      <FieldWrapper
+        label="Source URL"
+        required={true}
+        description="Input TAPIS file as a pathname, TAPIS URI or web URL"
+        error={errors['sourceUrl']}
+      >
+        <Input
+          bsSize="sm"
+          defaultValue={sourceUrl}
+          {...mapInnerRef(
+            register(`${refName}.sourceUrl`, {
+              required: 'Source URL is required',
+            })
+          )}
+        />
+      </FieldWrapper>
+      <FieldWrapper
+        label="Target Path"
+        required={true}
+        description="File mount path inside of running container"
+        error={errors['targetPath']}
+      >
+        <Input
+          bsSize="sm"
+          defaultValue={targetPath}
+          {...mapInnerRef(
+            register(`${refName}.targetPath`, {
+              required: 'Target Path is required',
+            })
+          )}
+        />
+      </FieldWrapper>
+      <FormGroup check>
+        <Label check className="form-field__label" size="sm">
+          <Input
+            type="checkbox"
+            bsSize="sm"
+            defaultChecked={inPlace}
+            {...mapInnerRef(register(`${refName}.inPlace`))}
+            className={styles['form-input-override']}
+          />{' '}
+          In Place
+        </Label>
+        <FormText className="form-field__help" color="muted">
+          If this is true, the source URL will be mounted from the execution
+          system's local file system
+        </FormText>
+      </FormGroup>
       {/* Possible metadata implementation
         <DictField refName={`${refName}.meta`} fieldSpecs={metaSpecs} {...rest} item={item.meta} />
       */}
