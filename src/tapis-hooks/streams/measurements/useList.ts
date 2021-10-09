7524f05349451e37b501c198868a743313784890
princessbobsanta <princessbobsantaclaus@gmail.com>
2021-10-08 15:29:10 -1000

    Revert "Update measurment infinite query hooks and migrate custom infinite query code to separate file"
    This reverts commit 7fdb8e540c01d65bf703c3b9b770678a0b6e5129.

--- src/tapis-hooks/streams/measurements/useList.ts
+++ src/tapis-hooks/streams/measurements/useList.ts
@@ -3,7 +3,7 @@ import { list } from 'tapis-api/streams/measurements';
 import { Streams } from '@tapis/tapis-typescript';
 import { useTapisConfig } from 'tapis-hooks';
 import QueryKeys from './queryKeys';
-import { concatResults, tapisNextPageParam } from './infiniteQuery';
+import { tapisNextPageParam } from 'tapis-hooks/utils/infiniteQuery';
 
 const useList = (params: Streams.ListMeasurementsRequest) => {
   const { accessToken, basePath } = useTapisConfig();
@@ -21,11 +21,91 @@ const useList = (params: Streams.ListMeasurementsRequest) => {
     () => list(params, basePath, accessToken?.access_token || ''),
     {
       getNextPageParam: (lastPage, allPages) => {
-        return tapisNextPageParam(lastPage, allPages, params);
+        return tapisNextPageParam<Streams.RespListMeasurements>(
+          lastPage,
+          allPages,
+          params
+        );
       },
       enabled: !!accessToken,
     }
   );
+
+  //Concatenate a set of Measurements objects from the same instrument
+  const concatResults = (
+    pages: Streams.RespListMeasurements[]
+  ): Streams.Measurements | null => {
+    //non data measurement properties
+    const nonDataProps = ['instrument', 'site', 'measurements_in_file'];
+
+    //measurements_in_file is a representation of the number of unique datetimes not the total measurements
+    //store datetimes in a set for uniqueness
+    let datetimes = new Set<string>();
+
+    //reduce to a single Measurements object
+    let reduced: Streams.Measurements | null = pages.reduce(
+      (
+        acc: Streams.Measurements | null,
+        item: Streams.RespListMeasurements
+      ) => {
+        let measurements: Streams.Measurements | null = item.result ?? null;
+        //if accumulator is not null add new measurements to accumulator
+        if (acc) {
+          for (let prop in measurements) {
+            //check if property is a variable field
+            if (!nonDataProps.includes(prop)) {
+              //get new variable measurements
+              let variableMeasurements: { [datetime: string]: number } =
+                measurements[prop];
+
+              //add datetimes to store
+              for (let datetime in variableMeasurements) {
+                datetimes.add(datetime);
+              }
+
+              //get variable measurements from accumulator
+              let accVariableMeasurements = acc[prop];
+              //combine objects if accumulator already has a reference to this variable
+              //otherwise just add new variable measurements to accumulator
+              acc[prop] = accVariableMeasurements
+                ? {
+                    ...accVariableMeasurements,
+                    ...variableMeasurements,
+                  }
+                : variableMeasurements;
+            }
+          }
+          //measurements in file is uniques datetimes?
+
+          //return updated accumulator
+          return acc;
+        }
+        //otherwise return measurements
+        else {
+          //still need to get datetimes
+          for (let prop in measurements) {
+            //check if property is a variable field
+            if (!nonDataProps.includes(prop)) {
+              //add datetimes to store
+              for (let datetime in measurements[prop]) {
+                datetimes.add(datetime);
+              }
+            }
+          }
+
+          return measurements;
+        }
+      },
+      null
+    );
+
+    //set measurements_in_file to the number of unique datetimes
+    if (reduced) {
+      reduced.measurements_in_file = datetimes.size;
+    }
+
+    return reduced;
+  };
 
   const concatenatedResults = result.data?.pages
     ? concatResults(result.data.pages)
