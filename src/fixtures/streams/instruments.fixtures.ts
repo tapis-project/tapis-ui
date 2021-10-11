450675a4071a41eea803ad35271b603630260af9
princessbobsanta <princessbobsantaclaus@gmail.com>
2021-10-11 10:42:34 -1000

    add fixtures

--- src/fixtures/streams/instruments.fixtures.ts
+++ src/fixtures/streams/instruments.fixtures.ts
@@ -1,6 +1,7 @@
 import { Streams } from '@tapis/tapis-typescript';
 
 export const instrument: Streams.Instrument = {
+<<<<<<< HEAD
   inst_id: 'ui_e2e_inst',
   inst_name: 'ui_e2e_inst',
   inst_description: 'ui_e2e_inst',
@@ -10,4 +11,15 @@ export const instrument: Streams.Instrument = {
   created: '2021-09-18T18:54:17Z',
   tags: [],
   metadata: {},
+=======
+  inst_id: "ui_e2e_inst",
+  inst_name: "ui_e2e_inst",
+  inst_description: "ui_e2e_inst",
+  chords_id: "",
+  owner: "ui_e2e_owner",
+  site_id: "ui_e2e_site",
+  created: "2021-09-18T18:54:17Z",
+  tags: [],
+  metadata: {}
+>>>>>>> cc44015 (add fixtures)
 };
--- src/fixtures/streams/sites.fixtures.ts
+++ src/fixtures/streams/sites.fixtures.ts
@@ -1,6 +1,7 @@
 import { Streams } from '@tapis/tapis-typescript';
 
 export const site: Streams.Site = {
+<<<<<<< HEAD
   site_name: 'ui_e2e_site',
   site_id: 'ui_e2e_site',
   owner: 'ui_e2e_owner',
@@ -8,4 +9,13 @@ export const site: Streams.Site = {
   latitude: 19.65,
   elevation: 10,
   metadata: {},
+=======
+  site_name: "ui_e2e_site",
+  site_id: "ui_e2e_site",
+  owner: "ui_e2e_owner",
+  longitude: -155.06,
+  latitude: 19.65,
+  elevation: 10,
+  metadata: {}
+>>>>>>> cc44015 (add fixtures)
 };
