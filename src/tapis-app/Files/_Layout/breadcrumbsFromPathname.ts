87659fe53fd4950f9f780803d5dcec3a6b7e616b
Nathan Freeman <nathan.dean.freeman@gmail.com>
2021-10-15 12:22:31 -0500

    Change map in breadcrumbsFromPathname to forEach to pass linting

--- src/tapis-app/Files/_Layout/breadcrumbsFromPathname.ts
+++ src/tapis-app/Files/_Layout/breadcrumbsFromPathname.ts
@@ -3,7 +3,7 @@ import { BreadcrumbType } from 'tapis-ui/_common/Breadcrumbs/Breadcrumbs';
 const breadcrumbsFromPathname = (pathname: string) => {
   const items: Array<BreadcrumbType> = [];
   const pathParts = pathname.split('/').slice(1, -1);
-  pathParts.map((_, index) => {
+  pathParts.forEach((_, index) => {
     const ds = index === 0 ? '' : '/';
     let to = '/' + pathParts.slice(0, index + 1).join('/') + ds;
     items.push({ to, text: pathParts[index] });
