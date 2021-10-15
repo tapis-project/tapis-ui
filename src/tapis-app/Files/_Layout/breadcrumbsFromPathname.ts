f86d7a8c44a3941898600a9ba16f923daa2fd026
Nathan Freeman <nathan.dean.freeman@gmail.com>
2021-10-15 11:59:05 -0500

    Create test for breadcrumbsFromPathaname function; Remove console.log statements

--- src/tapis-app/Files/_Layout/breadcrumbsFromPathname.ts
+++ src/tapis-app/Files/_Layout/breadcrumbsFromPathname.ts
@@ -1,6 +1,6 @@
 import { BreadcrumbType } from 'tapis-ui/_common/Breadcrumbs/Breadcrumbs';
 
-const breadcrumbFromPathname = (pathname: string) => {
+const breadcrumbsFromPathname = (pathname: string) => {
   const items: Array<BreadcrumbType> = [];
   const pathParts = pathname.split('/').slice(1, -1);
   pathParts.map((_, index) => {
@@ -8,8 +8,8 @@ const breadcrumbFromPathname = (pathname: string) => {
     let to = '/' + pathParts.slice(0, index + 1).join('/') + ds;
     items.push({ to, text: pathParts[index] });
   });
-  console.log(items);
+
   return items;
 };
 
-export default breadcrumbFromPathname;
+export default breadcrumbsFromPathname;
