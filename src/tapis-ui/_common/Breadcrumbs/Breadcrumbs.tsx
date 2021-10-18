8fbd7b068953cd2c33ccc5d3425ce9f7f62ca154
Nathan Freeman <nathan.dean.freeman@gmail.com>
2021-10-18 13:25:54 -0500

    Truncate breadcrumbs for 5 or more BreadcrumbTypes

--- src/tapis-ui/_common/Breadcrumbs/Breadcrumbs.tsx
+++ src/tapis-ui/_common/Breadcrumbs/Breadcrumbs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { range } from "tapis-ui/utils/range"
 import { NavLink } from 'react-router-dom';
 import styles from './Breadcrumbs.module.scss';
 
@@ -15,10 +16,27 @@ type BreadcrumbsProps = {
 const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
   breadcrumbs,
 }) => {
+
+  let truncatedIndices: Array<number> = []
+
+  if ( breadcrumbs.length >= 5 ) {
+    truncatedIndices = range(breadcrumbs.length-4, 2)
+    let filtered = breadcrumbs.filter((_, index) => {
+      if (truncatedIndices.includes(index)) {
+        return false
+      }
+
+      return true
+    })
+
+    filtered.splice(2, 0, {to: "", text: "..."})
+    breadcrumbs = filtered
+  }
+
   return (
     <div className={styles.root}>
       {breadcrumbs.map((item, index) => {
-        return index === breadcrumbs.length - 1 ? (
+        return index === breadcrumbs.length - 1 || item.text === "..." ? (
           <span> {item.text} /</span>
         ) : (
           <span>
