e862ed700794c76ff08cffe9569b8dc17c5b18e1
Nathan Dean Freeman <59402772+nathandf@users.noreply.github.com>
2021-10-19 14:38:16 -0500

    task/TUI-201 -- Add Breadcrumbs component, unit test, functions, and range util to Main branch (#202)
    * Add Breadcrumbs component, test, functions, and range util
    
    * Reduce complexity of breadcrumb truncation; Replace ... with \u2026
    
    * Remove range util; prettier
    
    * Add tests for 1 and 3 breadcrumbs; Add tests for text in component; Remove styles from Breadcrumbs.

---
+++ src/tapis-ui/_common/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { NavLink } from 'react-router-dom';
+
+export type BreadcrumbType = {
+  to: string;
+  text: string;
+};
+
+type BreadcrumbsProps = {
+  breadcrumbs: Array<BreadcrumbType>;
+};
+
+const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ breadcrumbs }) => {
+  if (breadcrumbs.length >= 5) {
+    // First 2 breadcrumbs
+    let truncatedBreadcrumbs = breadcrumbs.slice(0, 2);
+
+    // Ellipsis representing truncated breadcrumbs
+    truncatedBreadcrumbs.push({ to: '', text: '\u2026' });
+
+    // Last 2 breadcrumbs
+    truncatedBreadcrumbs.push(
+      ...breadcrumbs.slice(breadcrumbs.length - 2, breadcrumbs.length)
+    );
+    breadcrumbs = truncatedBreadcrumbs;
+  }
+
+  return (
+    <div>
+      {breadcrumbs.map((item, index) => {
+        return index === breadcrumbs.length - 1 || item.text === '\u2026' ? (
+          <span> {item.text} /</span>
+        ) : (
+          <span>
+            <NavLink to={item.to}> {item.text}</NavLink> /
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Breadcrumbs;
