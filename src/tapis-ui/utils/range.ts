84a8712908f155bb2e7278c80debfb67fb31573a
Nathan Freeman <nathan.dean.freeman@gmail.com>
2021-10-19 11:22:45 -0500

    Remove range util

--- src/tapis-ui/utils/range.ts
+++
@@ -1,7 +0,0 @@
-export const range = (size: number, startAt: number = 0): Array<number> => {
-  let rangeArr: Array<number> = [];
-  for (let i = 0; i < size; i++) {
-    rangeArr.push(i + startAt);
-  }
-  return rangeArr;
-};
