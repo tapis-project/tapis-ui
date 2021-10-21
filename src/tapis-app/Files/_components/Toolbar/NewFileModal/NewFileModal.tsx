69152b9e01489357808dc50b2815dbacd0bf9429
Nathan Freeman <nathan.dean.freeman@gmail.com>
2021-10-21 09:57:55 -0500

    Handle for submission with react query

--- src/tapis-app/Files/_components/Toolbar/NewFileModal/NewFileModal.tsx
+++ src/tapis-app/Files/_components/Toolbar/NewFileModal/NewFileModal.tsx
@@ -1,13 +1,32 @@
 import { Button, Input } from 'reactstrap';
 import { GenericModal, FieldWrapper } from 'tapis-ui/_common';
+import { SubmitWrapper } from 'tapis-ui/_wrappers';
 import { ToolbarModalProps } from '../Toolbar';
 import { useLocation } from 'react-router';
+import { useTapisConfig } from 'tapis-hooks';
+import { useForm } from 'react-hook-form';
 
 const NewFileModal: React.FC<ToolbarModalProps> = ({
   toggle,
   isOpen = false,
 }) => {
   const { pathname } = useLocation()
+  const { accessToken } = useTapisConfig()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: { foldername: null } });
+
+  const { ref: foldernameRef, ...foldernameFieldProps } = register('foldername', {
+    required: 'Folder name is a required field',
+  });
+
+  const onSubmit = ({
+    foldername,
+  }: {
+    foldername: string;
+  }) => alert(foldername);
 
   return (
     <GenericModal
@@ -16,23 +35,37 @@ const NewFileModal: React.FC<ToolbarModalProps> = ({
       title="New Folder"
       body={
         <div>
-          <form id="newfolder-form" onSubmit={() => {alert("Form submitted")}}>
+          <form id="newfolder-form" onSubmit={handleSubmit(onSubmit)}>
             <FieldWrapper
               label="Folder name"
               required={true}
               description={`New folder in ${pathname}`}
+              error={errors["foldername"]}
             >
-              <Input bsSize="sm" />
+              <Input
+                bsSize="sm"
+                {...foldernameFieldProps}
+                innerRef={foldernameRef}
+              />
             </FieldWrapper>
           </form>
         </div>
       }
       footer={
-        <Button
-          form="newfolder-form"
-          color="primary">
-            Create folder
+        <SubmitWrapper
+          isLoading={false}
+          error={null}
+          success={""}
+        >
+          <Button
+            form="newfolder-form"
+            color="primary"
+            disabled={false}
+          >
+              Create folder
           </Button>
+        </SubmitWrapper>
+        
       }
     />
   );
