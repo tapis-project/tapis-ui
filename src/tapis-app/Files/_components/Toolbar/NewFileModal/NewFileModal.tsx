4aa98a6a7a51b17006328d77cd4626b723dec7b9
Nathan Freeman <nathan.dean.freeman@gmail.com>
2021-10-21 09:20:19 -0500

    Add form components to newfolder form

--- src/tapis-app/Files/_components/Toolbar/NewFileModal/NewFileModal.tsx
+++ src/tapis-app/Files/_components/Toolbar/NewFileModal/NewFileModal.tsx
@@ -1,18 +1,39 @@
-import { Button } from 'reactstrap';
-import { GenericModal } from 'tapis-ui/_common';
+import { Button, Input } from 'reactstrap';
+import { GenericModal, FieldWrapper } from 'tapis-ui/_common';
 import { ToolbarModalProps } from '../Toolbar';
+import { useLocation } from 'react-router';
 
 const NewFileModal: React.FC<ToolbarModalProps> = ({
   toggle,
   isOpen = false,
 }) => {
+  const { pathname } = useLocation()
+
   return (
     <GenericModal
       isOpen={isOpen}
       toggle={toggle}
       title="New Folder"
-      body={<Button>Test</Button>}
-      footer={<Button>Test</Button>}
+      body={
+        <div>
+          <form id="newfolder-form" onSubmit={() => {alert("Form submitted")}}>
+            <FieldWrapper
+              label="Folder name"
+              required={true}
+              description={`New folder in ${pathname}`}
+            >
+              <Input bsSize="sm" />
+            </FieldWrapper>
+          </form>
+        </div>
+      }
+      footer={
+        <Button
+          form="newfolder-form"
+          color="primary">
+            Create folder
+          </Button>
+      }
     />
   );
 };
