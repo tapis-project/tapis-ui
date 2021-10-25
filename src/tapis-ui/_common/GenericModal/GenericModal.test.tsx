e977cd65e7ca1c859ec57f3958d4b767ccb4cc16
Nathan Freeman <nathan.dean.freeman@gmail.com>
2021-10-25 15:28:31 -0500

    GenericModal unit test

---
+++ src/tapis-ui/_common/GenericModal/GenericModal.test.tsx
@@ -0,0 +1,40 @@
+import '@testing-library/jest-dom/extend-expect';
+import renderComponent from 'utils/testing';
+import GenericModal from './GenericModal';
+
+jest.mock('tapis-hooks/systems');
+
+describe('GenericModal', () => {
+  it('renders GenericModal component with correct text and toggle function', () => {
+    const mockToggle = jest.fn();
+    const { getAllByText, getByLabelText } = renderComponent(
+      <GenericModal
+        toggle={mockToggle}
+        isOpen={true}
+        title="Generic Title"
+        body="Text in body"
+      />
+    );
+    expect(getAllByText(/Generic Title/).length).toEqual(1);
+    expect(getAllByText(/Text in body/).length).toEqual(1);
+
+    const closeButton = getByLabelText('Close');
+    closeButton.click();
+
+    expect(mockToggle).toBeCalledTimes(1);
+  });
+
+  it("Doesn't render GenericModal if isOpen set to false", () => {
+    const { queryAllByText } = renderComponent(
+      <GenericModal
+        toggle={() => {}}
+        isOpen={false}
+        title="Generic Title"
+        body="Text in body"
+      />
+    );
+
+    expect(queryAllByText(/Generic Title/).length).toEqual(0);
+    expect(queryAllByText(/Text in body/).length).toEqual(0);
+  });
+});
