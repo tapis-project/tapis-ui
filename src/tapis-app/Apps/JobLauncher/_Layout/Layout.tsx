714696c84add435b25d310f8b6f86363afb4adcb
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-01-03 21:38:32 -0600

    QueryWrapper around job launcher

--- src/tapis-app/Apps/JobLauncher/_Layout/Layout.tsx
+++ src/tapis-app/Apps/JobLauncher/_Layout/Layout.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { JobLauncher } from 'tapis-ui/components/jobs';
 import { PageLayout, LayoutHeader } from 'tapis-ui/_common';
 import { useDetail } from 'tapis-hooks/apps';
+import { QueryWrapper } from 'tapis-ui/_wrappers';
 
 const Layout: React.FC<{ appId: string; appVersion: string }> = ({
   appId,
   appVersion,
 }) => {
-  const { data: appData } = useDetail({ appId, appVersion });
-  const app = appData?.result;
+  const { data, isLoading, error } = useDetail({ appId, appVersion });
+  const app = data?.result;
 
   const header = <LayoutHeader type={'sub-header'}>Job Launcher</LayoutHeader>;
 
   const body = (
-    <div style={{ flex: 1 }}>{app && <JobLauncher app={app} />}</div>
+    <div style={{ flex: 1 }}>
+      <QueryWrapper isLoading={isLoading} error={error}>
+        <JobLauncher app={app!} />
+      </QueryWrapper>
+    </div>
   );
 
   return <PageLayout top={header} right={body} />;
