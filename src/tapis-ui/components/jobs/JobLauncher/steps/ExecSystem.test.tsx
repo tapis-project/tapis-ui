c8ba740fac1cd5624766179f47a8bd3e1f37f7b2
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-01-26 10:05:56 -0600

    Updated tests

--- src/tapis-ui/components/jobs/JobLauncher/steps/ExecSystem.test.tsx
+++ src/tapis-ui/components/jobs/JobLauncher/steps/ExecSystem.test.tsx
@@ -3,8 +3,23 @@ import { tapisSystem } from 'fixtures/systems.fixtures';
 import renderComponent from 'utils/testing';
 import { tapisApp } from 'fixtures/apps.fixtures';
 import { ExecSystem } from './ExecSystem';
+import { useFormContext } from 'react-hook-form';
+/* eslint-disable-next-line */
+import { mapInnerRef } from 'tapis-ui/utils/forms';
+
+jest.mock('react-hook-form');
+jest.mock('tapis-ui/utils/forms');
 
 describe('ExecSystem job launcher step', () => {
+  beforeEach(() => {
+    (useFormContext as jest.Mock).mockReturnValue({
+      register: jest.fn(),
+      formState: {
+        errors: {},
+      },
+      setValue: jest.fn(),
+    });
+  });
   it('loads the default queue', async () => {
     const systems = [tapisSystem];
     const { getAllByText } = renderComponent(
