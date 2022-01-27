fdcd8f48875408d38d7530f2c2d8bc1afb9d50f7
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-01-27 12:54:37 -0600

    Fix jobSlice undefined properties

--- src/tapis-hooks/jobs/jobLauncher/jobSlice.ts
+++ src/tapis-hooks/jobs/jobLauncher/jobSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Jobs } from '@tapis/tapis-typescript';
-import { parse } from 'path';
 
 export interface JobState {
   job: Partial<Jobs.ReqSubmitJob>
@@ -15,11 +14,19 @@ export const jobSlice = createSlice({
   initialState,
   reducers: {
     add: (state, action: PayloadAction<Partial<Jobs.ReqSubmitJob>>) => {
-      // Object deep copying is required due to array mutation restriction in RTK
-      state.job = {
+      // Create deep copy of original state, to prevent immer mutation problems
+      const newJob = {
         ...JSON.parse(JSON.stringify(state.job)),
-        ...JSON.parse(JSON.stringify(action.payload))
       }
+      // Allow key/value pairs to be undefined
+      for (const [key, value] of Object.entries(action.payload)) {
+        if (value === undefined) {
+          newJob[key] = undefined;
+        } else {
+          newJob[key] = JSON.parse(JSON.stringify(value));
+        }
+      }
+      state.job = newJob;
     },
     set: (state, action: PayloadAction<Partial<Jobs.ReqSubmitJob>>) => {
       state.job = action.payload
