1c417f82035379db7c8036942f3b2e79b8aad87c
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-02-01 11:45:37 -0600

    prettier

--- src/tapis-hooks/jobs/jobBuilder/jobBuilderSlice.test.ts
+++ src/tapis-hooks/jobs/jobBuilder/jobBuilderSlice.test.ts
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { default as jobBuilderReducer, add, set, clear } from './jobBuilderSlice';
+import {
+  default as jobBuilderReducer,
+  add,
+  set,
+  clear,
+} from './jobBuilderSlice';
 import '@testing-library/jest-dom/extend-expect';
 
 jest.mock('uuid');
@@ -17,16 +22,15 @@ describe('jobBuilderSlice', () => {
     expect(store.getState().job).toEqual({});
   });
   it('sets the job to a value', () => {
-    store.dispatch(set({ name: "testjob" }));
-    expect(store.getState().job).toEqual({ name: "testjob" });
+    store.dispatch(set({ name: 'testjob' }));
+    expect(store.getState().job).toEqual({ name: 'testjob' });
   });
   it('adds slices to a job', () => {
-    store.dispatch(set({ name: "testjob" }));
-    store.dispatch(add({ appId: "testapp" }));
-    expect(store.getState().job).toEqual({ name: "testjob", appId: "testapp" });
+    store.dispatch(set({ name: 'testjob' }));
+    store.dispatch(add({ appId: 'testapp' }));
+    expect(store.getState().job).toEqual({ name: 'testjob', appId: 'testapp' });
     // Verify that it unsets an undefined value
     store.dispatch(add({ appId: undefined }));
-    expect(store.getState().job).toEqual({ name: "testjob" });
+    expect(store.getState().job).toEqual({ name: 'testjob' });
   });
-
 });
