05ae0cd9c1e8158294e02991c6ddc84cd0b9d85e
jchuahtacc <jchuah@tacc.utexas.edu>
2022-04-06 09:05:23 -0500

    task/TUI-264 -- app args (#292)
    * Env Variables
    
    * linting
    
    * jobAppArg utils
    
    * App args
    
    * Bug fix: formik checks
    
    * Disable fields if app argument specified in app
    
    * Generalize Args
    
    * Generalize app field array
    
    * Add other argument types
    
    * Change checkbox message
    
    * Fix addition of new args

--- src/tapis-ui/components/jobs/JobLauncher/JobLauncherWizard.tsx
+++ src/tapis-ui/components/jobs/JobLauncher/JobLauncherWizard.tsx
@@ -9,6 +9,7 @@ import { ExecSystem, ExecSystemSummary } from './steps/ExecSystem';
 import { JobSubmission, JobSubmissionSummary } from './steps/JobSubmission';
 import { ArchiveFilter, ArchiveFilterSummary } from './steps/ArchiveFilter';
 import { EnvVariables, EnvVariablesSummary } from './steps/EnvVariables';
+import { Args, ArgsSummary } from './steps/AppArgs';
 import {
   FileInputArrays,
   FileInputArraysSummary,
@@ -21,6 +22,7 @@ import {
   generateRequiredFileInputArraysFromApp,
   fileInputArraysComplete,
 } from 'tapis-api/utils/jobFileInputArrays';
+import { generateJobArgsFromSpec } from 'tapis-api/utils/jobArgs';
 import { jobRequiredFieldsComplete } from 'tapis-api/utils/jobRequiredFields';
 import { Button } from 'reactstrap';
 import { useSubmit } from 'tapis-hooks/jobs';
@@ -50,6 +52,15 @@ const generateDefaultValues = (
     fileInputs: generateRequiredFileInputsFromApp(app),
     fileInputArrays: generateRequiredFileInputArraysFromApp(app),
     parameterSet: {
+      appArgs: generateJobArgsFromSpec(
+        app.jobAttributes?.parameterSet?.appArgs ?? []
+      ),
+      containerArgs: generateJobArgsFromSpec(
+        app.jobAttributes?.parameterSet?.containerArgs ?? []
+      ),
+      schedulerOptions: generateJobArgsFromSpec(
+        app.jobAttributes?.parameterSet?.schedulerOptions ?? []
+      ),
       archiveFilter: app.jobAttributes?.parameterSet?.archiveFilter,
       envVariables: app.jobAttributes?.parameterSet?.envVariables,
     },
@@ -138,6 +149,12 @@ const JobLauncherWizard: React.FC<JobLauncherWizardProps> = ({
       name: 'File Input Arrays',
       render: <FileInputArrays />,
       summary: <FileInputArraysSummary />,
+    },
+    {
+      id: 'args',
+      name: 'Arguments',
+      render: <Args />,
+      summary: <ArgsSummary />,
     },
     {
       id: 'envVariables',
