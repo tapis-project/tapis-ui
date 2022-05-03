a81ffbac03bfda9cc7335de50825cca67f0bde09
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-05-03 13:14:36 -0500

    Job submission step

--- src/tapis-ui/components/jobs/JobLauncher/JobLauncherWizard.tsx
+++ src/tapis-ui/components/jobs/JobLauncher/JobLauncherWizard.tsx
@@ -1,21 +1,15 @@
 import React, { useCallback, useMemo } from 'react';
 import { WizardStep } from 'tapis-ui/_wrappers/Wizard';
 import { QueryWrapper, Wizard } from 'tapis-ui/_wrappers';
-import { WizardSubmitWrapper } from 'tapis-ui/_wrappers/Wizard';
 import { Apps, Jobs, Systems } from '@tapis/tapis-typescript';
 import {
   generateRequiredFileInputsFromApp,
-  fileInputsComplete,
 } from 'tapis-api/utils/jobFileInputs';
 import {
   generateRequiredFileInputArraysFromApp,
-  fileInputArraysComplete,
 } from 'tapis-api/utils/jobFileInputArrays';
-
 import { generateJobArgsFromSpec } from 'tapis-api/utils/jobArgs';
-import { jobRequiredFieldsComplete } from 'tapis-api/utils/jobRequiredFields';
-import { Button } from 'reactstrap';
-import { useSubmit } from 'tapis-hooks/jobs';
+
 import { useDetail as useAppDetail } from 'tapis-hooks/apps';
 import {
   useList as useSystemsList,
@@ -80,37 +74,6 @@ const generateDefaultValues = ({
   return defaultValues;
 };
 
-const JobLauncherWizardSubmit: React.FC<{ app: Apps.TapisApp }> = ({ app }) => {
-  const { job } = useJobLauncher();
-  const isComplete =
-    jobRequiredFieldsComplete(job) &&
-    fileInputsComplete(app, job.fileInputs ?? []) &&
-    fileInputArraysComplete(app, job.fileInputArrays ?? []);
-  const { isLoading, error, isSuccess, submit, data } = useSubmit(
-    app.id!,
-    app.version!
-  );
-  const onSubmit = useCallback(() => {
-    submit(job as Jobs.ReqSubmitJob);
-  }, [submit, job]);
-  return (
-    <WizardSubmitWrapper
-      isLoading={isLoading}
-      error={error}
-      success={
-        isSuccess
-          ? `Successfully submitted job ${data?.result?.uuid ?? ''}`
-          : ''
-      }
-      title={`Job Submission Summary`}
-    >
-      <Button disabled={!isComplete} color="primary" onClick={onSubmit}>
-        Submit
-      </Button>
-    </WizardSubmitWrapper>
-  );
-};
-
 const JobLauncherWizardRender: React.FC = () => {
   const { add, job, app, systems } = useJobLauncher();
 
@@ -145,37 +108,11 @@ const JobLauncherWizardRender: React.FC = () => {
     ),
     [ app, job, systems ]
   )
-  /*
-  const steps: Array<WizardStep<Jobs.ReqSubmitJob>> = useMemo(() => [
-    {
-      id: 'schedulerOptions',
-      name: 'Scheduler Options',
-      render: <SchedulerOptions />,
-      summary: <SchedulerOptionsSummary />,
-    },
-    {
-      id: 'archiving',
-      name: 'Archiving',
-      render: <Archive />,
-      summary: <ArchiveSummary />,
-    },
-    {
-      id: 'jobJson',
-      name: 'Job JSON',
-      render: <JobJson />,
-      summary: <JobJsonSummary />,
-      validationSchema: {},
-      initialValues: {}
-    },
-    ],
-    [ job, app, systems ]
-  );
-  */
+
   return (
     <Wizard
       steps={steps}
       memo={`${app.id}${app.version}`}
-      renderSubmit={<JobLauncherWizardSubmit app={app} />}
       formSubmit={formSubmit}
     />
   )
--- src/tapis-ui/components/jobs/JobLauncher/steps/index.ts
+++ src/tapis-ui/components/jobs/JobLauncher/steps/index.ts
@@ -3,11 +3,11 @@ import ExecOptions from './ExecOptions';
 import FileInputs from './FileInputs';
 import FileInputArrays from './FileInputArrays';
 import AppArgs from './AppArgs';
-import JobJson from './JobJson';
 import EnvVariables from './EnvVariables';
 import SchedulerOptions from './SchedulerOptions';
 import Archive from './Archive';
+import JobSubmit from './JobSubmit';
 
-const jobSteps = [ JobStart, ExecOptions, FileInputs, FileInputArrays, AppArgs, EnvVariables, SchedulerOptions, Archive, JobJson ];
+const jobSteps = [ JobStart, ExecOptions, FileInputs, FileInputArrays, AppArgs, EnvVariables, SchedulerOptions, Archive, JobSubmit ];
 
 export default jobSteps;
