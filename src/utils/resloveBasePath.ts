04f82e66ff4e34e4838ee1c538622dbb7b83363e
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-01-09 13:55:35 -0600

    fix resolveBasePath

--- src/utils/resloveBasePath.ts
+++ src/utils/resloveBasePath.ts
@@ -1,7 +1,7 @@
 export const resolveBasePath = () => {
-  let baseUrl = window.location.href.split("/#")[0]
+  let baseUrl = window.location.href
+    .split('/#')[0]
     .replace(/^https:\/\/ui\./, 'https://');
-  console.log({baseUrl})
   // Direct request from local dev env to dev.develop
   if (/127\.0\.0\.1|localhost|0\.0\.0\.0/.test(baseUrl)) {
     return 'https://dev.develop.tapis.io';
