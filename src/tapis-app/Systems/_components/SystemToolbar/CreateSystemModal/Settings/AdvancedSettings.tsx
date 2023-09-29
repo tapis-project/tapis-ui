5b2c69dd0e01f42fbc523d6bd1a99e3e51ab9e30
Steven Oh <91847206+jaeestee@users.noreply.github.com>
2023-09-29 12:23:15 -0500

    Added ability to build a system through Tapis UI (#341)
    * Added ability to build a system through Tapis UI
    
    * ran prettier and fixed linting issues
    
    * added a comment to the effectiveUserId
    
    ---------
    
    Co-authored-by: Steven Oh <potato@wireless-10-147-22-241.public.utexas.edu>
    Co-authored-by: Steven Oh <potato@Stevens-MacBook-Pro.local>

---
+++ src/tapis-app/Systems/_components/SystemToolbar/CreateSystemModal/Settings/AdvancedSettings.tsx
@@ -0,0 +1,103 @@
+import { FormikInput, Collapse } from 'tapis-ui/_common';
+import { FormikSelect } from 'tapis-ui/_common/FieldWrapperFormik';
+import { RuntimeTypeEnum } from '@tapis/tapis-typescript-systems';
+import { Systems } from '@tapis/tapis-typescript';
+import { useMemo } from 'react';
+import { SystemTypeEnum } from '@tapis/tapis-typescript-systems';
+import { useFormikContext } from 'formik';
+import styles from '../CreateSystemModal.module.scss';
+import BatchSettings from './Batch/BatchSettings';
+import ProxySettings from './ProxySettings';
+import DtnSettings from './DtnSettings';
+import CmdSettings from './CmdSettings';
+import TagsSettings from './TagsSettings';
+import JobSettings from './Job/JobSettings';
+
+//Array that is used in the drop-down menus
+const runtimeTypes = Object.values(RuntimeTypeEnum);
+
+type AdvancedSettingsProp = {
+  simplified: boolean;
+};
+
+const AdvancedSettings: React.FC<AdvancedSettingsProp> = ({ simplified }) => {
+  //used when trying to read the current value of a parameter
+  const { values } = useFormikContext();
+
+  //reading if the systemType is S3 at its current state
+  const isS3 = useMemo(
+    () =>
+      (values as Partial<Systems.ReqCreateSystem>).systemType ===
+      SystemTypeEnum.S3,
+    [values]
+  );
+
+  //reading the runtimeType at its current state
+  const runtimeType = (values as Partial<Systems.ReqCreateSystem>).jobRuntimes;
+
+  if (simplified) {
+    return (
+      <Collapse
+        title="Advanced Settings"
+        className={styles['item']}
+        open={true}
+      >
+        <FormikInput
+          name="rootDir"
+          label="Root Directory"
+          required={false}
+          description={`Root directory`}
+          aria-label="Input"
+        />
+        <FormikSelect
+          name="jobRuntimes"
+          description="The job runtime type for the system"
+          label="Runtime Type"
+          required={false}
+          data-testid="jobRuntimes"
+        >
+          <option disabled value="">
+            Select a job runtime
+          </option>
+          {runtimeTypes.map((values) => {
+            return <option>{values}</option>;
+          })}
+        </FormikSelect>
+        <FormikInput
+          name="version"
+          label={`${runtimeType} Version`}
+          required={false}
+          description={`Version of ${runtimeType}`}
+          aria-label="Input"
+          disabled={true}
+        />
+        <FormikInput
+          name="effectiveUserId"
+          label="Effective User ID"
+          required={false}
+          description={`Effective user id`}
+          aria-label="Input"
+        />
+        {isS3 ? (
+          <FormikInput
+            name="bucketName"
+            label="Bucket Name"
+            required={false}
+            description={`Bucket name`}
+            aria-label="Input"
+          />
+        ) : null}
+        <JobSettings />
+        <BatchSettings />
+        <ProxySettings />
+        <DtnSettings />
+        <CmdSettings />
+        <TagsSettings />
+      </Collapse>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default AdvancedSettings;
--- src/tapis-hooks/systems/index.ts
+++ src/tapis-hooks/systems/index.ts
@@ -1,3 +1,4 @@
 export { default as useList } from './useList';
 export { default as useDetails } from './useDetails';
 export { default as useSchedulerProfiles } from './useSchedulerProfiles';
+export { default as useMakeNewSystem } from './useMakeNewSystem';
