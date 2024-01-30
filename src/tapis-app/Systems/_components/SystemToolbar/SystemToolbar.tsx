d0829237b0b8bc00cf0d417e521439f48145880f
Steven Oh <91847206+jaeestee@users.noreply.github.com>
2024-01-29 23:31:07 -0600

    Pushing previous ability to create a system, fixes of jobLauncher, and new Delete and Undelete functionality to Tapis UI (#346)
    * Added ability to build a system through Tapis UI
    
    * ran prettier and fixed linting issues
    
    * added a comment to the effectiveUserId
    
    * fixed JobLauncher taking in inputs as string and not  number
    
    * added Delete and UnDelete System Functionality
    
    * fixed linting issue with SystemToolbar.tsx
    
    ---------
    
    Co-authored-by: Steven Oh <potato@wireless-10-147-22-241.public.utexas.edu>
    Co-authored-by: Steven Oh <potato@Stevens-MacBook-Pro.local>
    Co-authored-by: Steven Oh <potato@wireless-10-154-3-83.public.utexas.edu>

--- src/tapis-app/Systems/_components/SystemToolbar/SystemToolbar.tsx
+++ src/tapis-app/Systems/_components/SystemToolbar/SystemToolbar.tsx
@@ -4,6 +4,8 @@ import { Icon } from 'tapis-ui/_common';
 import styles from './SystemToolbar.module.scss';
 import { useLocation } from 'react-router-dom';
 import CreateSystemModal from './CreateSystemModal';
+import DeleteSystemModal from './DeleteSystemModal';
+import UndeleteSystemModal from './UndeleteSystemModal';
 
 type ToolbarButtonProps = {
   text: string;
@@ -56,8 +58,26 @@ const SystemToolbar: React.FC = () => {
             onClick={() => setModal('createsystem')}
             aria-label="createSystem"
           />
+          <ToolbarButton
+            text="Delete a System"
+            icon="trash"
+            disabled={false}
+            onClick={() => setModal('deletesystem')}
+            aria-label="deleteSystem"
+          />
+          <ToolbarButton
+            text="Re-Add a System"
+            icon="add"
+            disabled={false}
+            onClick={() => setModal('undeletesystem')}
+            aria-label="undeleteSystem"
+          />
 
           {modal === 'createsystem' && <CreateSystemModal toggle={toggle} />}
+          {modal === 'deletesystem' && <DeleteSystemModal toggle={toggle} />}
+          {modal === 'undeletesystem' && (
+            <UndeleteSystemModal toggle={toggle} />
+          )}
         </div>
       )}
     </div>
--- src/tapis-hooks/systems/index.ts
+++ src/tapis-hooks/systems/index.ts
@@ -1,4 +1,7 @@
 export { default as useList } from './useList';
+export { default as useDeletedList } from './useDeletedList';
 export { default as useDetails } from './useDetails';
 export { default as useSchedulerProfiles } from './useSchedulerProfiles';
 export { default as useMakeNewSystem } from './useMakeNewSystem';
+export { default as useDeleteSystem } from './useDeleteSystem';
+export { default as useUndeleteSystem } from './useUndeleteSystem';
