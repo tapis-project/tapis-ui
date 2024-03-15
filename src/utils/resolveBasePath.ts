e7b5735ecec2162a452bb36c1bbaefd10ad99f7a
Christian R. Garcia <christian.g21@gmail.com>
2024-03-15 13:34:20 -0700

    linting

--- src/utils/resolveBasePath.ts
+++ src/utils/resolveBasePath.ts
@@ -1,10 +1,9 @@
 export const resolveBasePath = () => {
-
   let baseUrl = window.location.origin;
 
   // Direct request from local dev env to dev.develop
   if (/127\.0\.0\.1|localhost|0\.0\.0\.0/.test(baseUrl)) {
-   return 'https://dev.develop.tapis.io';
+    return 'https://dev.develop.tapis.io';
   }
 
   return baseUrl;
