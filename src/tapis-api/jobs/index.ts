2fa119256b5521562eb946b83d5a715a33db064e
Steven Oh <oh.jae.steven@utexas.edu>
2024-04-03 23:48:04 -0500

    added hide and unhide job functionality

--- src/tapis-api/jobs/index.ts
+++ src/tapis-api/jobs/index.ts
@@ -2,3 +2,5 @@ export { default as list } from './list';
 export { default as details } from './details';
 export { default as submit } from './submit';
 export { default as cancel } from './cancel';
+export { default as hideJob } from './hideJob';
+export { default as unhideJob } from './unhideJob';
--- src/tapis-hooks/jobs/index.ts
+++ src/tapis-hooks/jobs/index.ts
@@ -2,3 +2,5 @@ export { default as useList } from './useList';
 export { default as useDetails } from './useDetails';
 export { default as useSubmit } from './useSubmit';
 export { default as useCancel } from './useCancel';
+export { default as useHideJob } from './useHideJob';
+export { default as useUnhideJob } from './useUnhideJob';
