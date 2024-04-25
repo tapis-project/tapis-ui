473609fb28e4a22d1224e997d3ccb1612d8a2e41
dhannywi <dwindrakusuma@gmail.com>
2024-04-25 13:30:32 -0500

    create api calls for fetch inference server availability and fetch download links

---
+++ src/tapis-api/ml-hub/models/downloadLinks.ts
@@ -0,0 +1,18 @@
+import { Models } from '@tapis/tapis-typescript';
+import { apiGenerator, errorDecoder } from 'tapis-api/utils';
+
+const downloadLinks = (
+  params: Models.DownloadModelRequest,
+  basePath: string,
+  jwt: string
+) => {
+  const api: Models.ModelsApi = apiGenerator<Models.ModelsApi>(
+    Models,
+    Models.ModelsApi,
+    basePath,
+    jwt
+  );
+  return errorDecoder<Models.RespModelDownload>(() => api.downloadModel(params));
+};
+
+export default downloadLinks;
