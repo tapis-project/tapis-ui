61da3d94f5e7974911ce1f34dc1ec3a68476efb2
dhannywi <dwindrakusuma@gmail.com>
2024-04-25 13:55:14 -0500

    api calls to filter models by specified parameters

---
+++ src/tapis-api/ml-hub/models/index.ts
@@ -0,0 +1,9 @@
+export { default as list } from './list';
+export { default as details } from './details';
+export { default as downloadLinks } from './downloadLinks';
+export { default as listByAuthor } from './listByAuthor';
+export { default as listByDataset } from './listByDataset';
+export { default as listByLanguage } from './listByLanguage';
+export { default as listByLibrary } from './listByLibrary';
+export { default as listByQuery } from './listByQuery';
+export { default as listByTask } from './listByTask';
---
+++ src/tapis-api/ml-hub/models/listByTask.ts
@@ -0,0 +1,18 @@
+import { Models } from '@tapis/tapis-typescript';
+import { apiGenerator, errorDecoder } from 'tapis-api/utils';
+
+const listByTask = (
+  params: Models.ListModelsByTaskRequest,
+  basePath: string,
+  jwt: string
+) => {
+  const api: Models.ModelsApi = apiGenerator<Models.ModelsApi>(
+    Models,
+    Models.ModelsApi,
+    basePath,
+    jwt
+  );
+  return errorDecoder<Models.RespModelObject>(() => api.listModelsByTask(params));
+};
+
+export default listByTask;
