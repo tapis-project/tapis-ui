20e552fb5a871f1d2621c6a64a5700dcd6d9b415
dhannywi <dwindrakusuma@gmail.com>
2024-04-25 19:03:00 -0500

    assign queryKeys and create hooks to fetch 100 top ml models and detailed info for each model

---
+++ src/tapis-hooks/ml-hub/models/queryKeys.ts
@@ -0,0 +1,13 @@
+const QueryKeys = {
+    list: 'ml-hub/models/list',
+    details: 'ml-hub/models/details',
+    downloadLinks: 'ml-hub/models/downloadLinks',
+    listByAuthor: 'ml-hub/models/listByAuthor',
+    listByDataset: 'ml-hub/models/listByDataset',
+    listByLanguage: 'ml-hub/models/listByLanguage',
+    listByLibrary: 'ml-hub/models/listByLibrary',
+    listByQuery: 'ml-hub/models/listByQuery',
+    listByTask: 'ml-hub/models/listByTask',
+  };
+  
+  export default QueryKeys;
