743ccceed8bc94f0b66131fe84c69c013f005bd6
Maximiliano Osorio <maxiosorio@gmail.com>
2024-05-08 14:52:01 -0400

    fix: update path

--- src/tapis-ui/_common/Breadcrumbs/breadcrumbsFromPathname.ts
+++ src/tapis-ui/_common/Breadcrumbs/breadcrumbsFromPathname.ts
@@ -1,11 +1,14 @@
-import { BreadcrumbType } from 'tapis-ui/_common/Breadcrumbs/Breadcrumbs';
-import { normalize } from 'path';
+import { BreadcrumbType } from "tapis-ui/_common/Breadcrumbs/Breadcrumbs";
+import normalize from "normalize-path";
 
 const breadcrumbsFromPathname = (pathname: string) => {
   const items: Array<BreadcrumbType> = [];
-  const pathParts = pathname.split('/');
+  const pathParts = pathname.split("/");
   pathParts.forEach((_, index) => {
-    let to = normalize('/' + pathParts.slice(0, index + 1).join('/') + '/');
+    const to = normalize(
+      "/" + pathParts.slice(0, index + 1).join("/") + "/",
+      false
+    );
     if (pathParts[index].length) {
       items.push({ to, text: pathParts[index] });
     }
