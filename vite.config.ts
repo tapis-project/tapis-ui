580ceb007cc3c6c984187ed6ce3b469e1189f709
Maximiliano Osorio <maxiosorio@gmail.com>
2024-05-08 12:10:36 -0400

    fix: add process env

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
+const env = loadEnv("", process.cwd());
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: "",
   css: { preprocessorOptions: { scss: { charset: false } } },
+  define: {
+    "process.env": env,
+  },
   plugins: [react(), viteTsconfigPaths(), viteTsconfigPaths()],
   server: {
     // this ensures that the browser opens upon server start
