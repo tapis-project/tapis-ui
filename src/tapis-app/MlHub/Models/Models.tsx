c024223505e1bce947617eb3087084394b7a4b3d
Gilbert Curbelo <gilbertcurbelo3@gmail.com>
2024-05-23 13:04:49 -0500

    Match models icon, add ellipsis if name is too long

--- src/tapis-app/MlHub/Models/Models.tsx
+++ src/tapis-app/MlHub/Models/Models.tsx
@@ -5,40 +5,41 @@ import { useList } from 'tapis-hooks/ml-hub/models';
 import { Icon } from 'tapis-ui/_common';
 import { QueryWrapper } from 'tapis-ui/_wrappers';
 import { Table } from 'reactstrap';
+import styles from './Models.module.scss';
 
 const Models: React.FC = () => {
-  const { data, isLoading, error } = useList();
-  const models: ModelsModule.ModelShortInfo = data?.result ?? {};
+    const { data, isLoading, error } = useList();
+    const models: ModelsModule.ModelShortInfo = data?.result ?? {};
 
-  return (
-    <QueryWrapper isLoading={isLoading} error={error}>
-      <Table responsive striped>
-        <thead>
-          <tr>
-            <th>Model ID</th>
-            <th>Task</th>
-            <th>Downloads</th>
-            <th>Last Modified</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.entries(models).map((model) => (
-            <tr>
-              <td>
-                <Icon name="gear" />
-                <span>
-                  <Link to={`/ml-hub/models/${model[0]}`}> {model[0]} </Link>
-                </span>
-              </td>
-              <td>{model[1].pipeline_tag ? model[1].pipeline_tag : 'None'}</td>
-              <td>{model[1].downloads}</td>
-              <td>{model[1].last_modified}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </QueryWrapper>
-  );
+    return (
+        <QueryWrapper isLoading={isLoading} error={error}>
+            <Table responsive striped>
+                <thead>
+                    <tr>
+                        <th>Model ID</th>
+                        <th>Task</th>
+                        <th>Downloads</th>
+                        <th>Last Modified</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.entries(models).map((model) => (
+                        <tr>
+                            <td className={`${styles['model-name-column']}`}>
+                                <Icon name="simulation" />
+                                <span>
+                                    <Link to={`/ml-hub/models/${model[0]}`}> {model[0]} </Link>
+                                </span>
+                            </td>
+                            <td>{model[1].pipeline_tag ? model[1].pipeline_tag : 'None'}</td>
+                            <td>{model[1].downloads}</td>
+                            <td>{model[1].last_modified}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </QueryWrapper>
+    );
 };
 
 export default Models;
