18bfed6f37e53d83bcfeaca394af84d50154af87
Nathan Freeman <nathan.dean.freeman@gmail.com>
2024-06-21 14:20:31 -0500

    add test-all script to run external packages tests. Update github workflows

---
+++ scripts/test-all.sh
@@ -0,0 +1,16 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$(dirname $SCRIPT_DIR)
+LIB_DIR="${ROOT_DIR}/lib"
+
+runTest() {
+  # Run the api tests
+  cd $1
+  npm run test
+  testExitCode=$?
+  if [[ $testExitCode != 0 ]]; then
+    exit $testExitCode
+  fi;
+  cd $ROOT_DIR
+}
+
+runTest lib/tapisui-hooks && runTest lib/tapisui-api # && runTest lib/tapisui-common
