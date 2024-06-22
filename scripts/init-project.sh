3d13a1b510edbd58b2ca6de13d787f8d22c1039f
Nathan Freeman <nathan.dean.freeman@gmail.com>
2024-06-21 21:43:50 -0500

    Explore query client error. Adjust init script

--- scripts/init-project.sh
+++ scripts/init-project.sh
@@ -25,30 +25,40 @@ fi;
 
 cd lib/tapisui-api
 echo $(pwd)
+rm -rf dist node_modules
+rm package-lock.json
 npm install
 npm run build
 cd ../../
 
 cd lib/tapisui-hooks
 echo $(pwd)
+rm -rf dist node_modules
+rm package-lock.json
 npm install
 npm run build
 cd ../../
 
 cd lib/tapisui-common
 echo $(pwd)
+rm -rf dist node_modules
+rm package-lock.json
 npm install
 npm run build
 cd ../../
 
 cd lib/tapisui-extensions-core
 echo $(pwd)
+rm -rf dist node_modules
+rm package-lock.json
 npm install
 npm run build
 cd ../../
 
 cd lib/icicle-tapisui-extension
 echo $(pwd)
+rm -rf dist node_modules
+rm package-lock.json
 npm install
 npm run build
 cd ../../
