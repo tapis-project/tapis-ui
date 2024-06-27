63ad6dc5ea444bc08b9c67a82c32d20d30488006
Nathan Freeman <nathan.dean.freeman@gmail.com>
2024-06-27 12:01:47 -0500

    Fix ProtectedRoute types

--- lib/tapisui-common/src/ui/ProtectedRoute/ProtectedRoute.tsx
+++ lib/tapisui-common/src/ui/ProtectedRoute/ProtectedRoute.tsx
@@ -3,14 +3,15 @@ import { Route, Redirect, RouteComponentProps } from 'react-router-dom';
 
 // A wrapper for <Route> that redirects to the login
 // screen if you're not yet authenticated.
-type ProtectedRouteProps = RouteComponentProps & { accessToken: string };
+type ProtectedRouteProps = RouteComponentProps & { accessToken: string | undefined, path: string };
 
 const ProtectedRoute: React.FC<
   React.PropsWithChildren<ProtectedRouteProps>
-> = ({ accessToken, children, ...rest }) => {
+> = ({ accessToken, path, children, ...rest }) => {
   return (
     <Route
       {...rest}
+      path={path}
       render={({ location }: RouteComponentProps) =>
         accessToken ? (
           children
