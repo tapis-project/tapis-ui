19ff0d7e409cd5694f17c143a18b4e49a8e2a1e8
dhannywi <dwindrakusuma@gmail.com>
2024-07-16 19:09:46 +0000

    remove explicit basePath from ML Hub datasets so it will read basePath implicitly from  or deployment url

--- lib/tapisui-hooks/src/ml-hub/datasets/useCardDetails.ts
+++ lib/tapisui-hooks/src/ml-hub/datasets/useCardDetails.ts
@@ -3,14 +3,12 @@ import { MLHub as API } from '@tapis/tapisui-api';
 import { Datasets } from '@tapis/tapis-typescript';
 import { useTapisConfig } from '../../';
 import QueryKeys from './queryKeys';
-import basePath from './basePath'; // remove if ml-hub is listed in NGINX
 
 const useCardDetails = (
   params: Datasets.GetDatasetCardRequest,
   options: QueryObserverOptions<Datasets.RespDatasetCard, Error> = {}
 ) => {
-  // const { accessToken, basePath } = useTapisConfig();
-  const { accessToken } = useTapisConfig(); // remove this line and uncomment line above if ml-hub is listed in NGINX
+  const { accessToken, basePath } = useTapisConfig();
   const result = useQuery<Datasets.RespDatasetCard, Error>(
     [QueryKeys.details, params, accessToken],
     // Default to no token. This will generate a 403 when calling the list function
