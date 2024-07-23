366b341a40174b8753bc5c350b34df9907e7646c
Rahil Ashtari Mahini <ramahini@wireless-10-148-40-110.public.utexas.edu>
2024-07-23 15:06:47 -0500

    Updated mlhub router-3

--- src/app/MlHub/_Router/Router.tsx
+++ src/app/MlHub/_Router/Router.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { Route, useRouteMatch, Switch } from 'react-router-dom';
 import { Dashboard } from '../Dashboard';
 import { Layout as ModelsLayout } from '../Models/_Layout';
-<<<<<<< HEAD
 import { Layout as DatasetsLayout } from '../Datasets/_Layout';
-=======
-import { Layout as DatasetsLayout } from "../Datasets/_Layout"
->>>>>>> 96485ea75d8db4b48e864d20161e1a2e52551576
 
 const Router: React.FC = () => {
   const { path } = useRouteMatch();
@@ -19,14 +15,8 @@ const Router: React.FC = () => {
       <Route path={`${path}/models`}>
         <ModelsLayout />
       </Route>
-<<<<<<< HEAD
 
       <Route path={`${path}/datasets`}>
-=======
-      
-      <Route
-        path={`${path}/datasets`}>
->>>>>>> 96485ea75d8db4b48e864d20161e1a2e52551576
         <DatasetsLayout />
       </Route>
     </Switch>
