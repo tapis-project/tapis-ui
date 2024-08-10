2fa0efe67d211191ea5942af9e171dfbbb68c513
Nathan Freeman <nathan.dean.freeman@gmail.com>
2024-08-10 18:58:12 -0500

    prettier

--- packages/icicle-tapisui-extension/src/gen/index.ts
+++ packages/icicle-tapisui-extension/src/gen/index.ts
@@ -1,3 +1,5 @@
-import { Workflows } from "@tapis/tapis-typescript"
-import { task as task0 } from "./django-search"
-export const tasks: Array<Workflows.FunctionTask> = [Workflows.FunctionTaskFromJSON(task0),]
+import { Workflows } from '@tapis/tapis-typescript';
+import { task as task0 } from './django-search';
+export const tasks: Array<Workflows.FunctionTask> = [
+  Workflows.FunctionTaskFromJSON(task0),
+];
