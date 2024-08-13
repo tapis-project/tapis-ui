3c5ba98aa4bd3cc50e85865efdfcf519f52e59a1
Nathan Freeman <nathan.dean.freeman@gmail.com>
2024-08-13 10:20:19 -0500

    prettier

--- src/app/Workflows/Pipelines/PipelineRuns/PipelineRuns.tsx
+++ src/app/Workflows/Pipelines/PipelineRuns/PipelineRuns.tsx
@@ -6,7 +6,13 @@ import { QueryWrapper } from '@tapis/tapisui-common';
 import styles from './PipelineRuns.module.scss';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { ExpandMore } from '@mui/icons-material';
-import { Accordion, AccordionDetails, AccordionSummary, Button, AccordionActions } from '@mui/material';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  AccordionActions,
+} from '@mui/material';
 import { Table } from 'reactstrap';
 
 type PipelineRunProps = {
@@ -31,18 +37,20 @@ const PipelineRuns: React.FC<PipelineRunsProps> = ({ groupId, pipelineId }) => {
     a.started_at! < b.started_at! ? 1 : a.started_at! > b.started_at! ? -1 : 0
   );
   const [showModal, setShowModal] = useState<boolean>(false);
-  const history = useHistory()
-  const { pathname } = useLocation()
+  const history = useHistory();
+  const { pathname } = useLocation();
 
   return (
     <div className={styles['grid']}>
       <QueryWrapper isLoading={isLoading} error={error}>
-        <div id="pipelineruns" className={styles["runs"]}>
+        <div id="pipelineruns" className={styles['runs']}>
           {pipelineRuns.length ? (
             pipelineRuns.map((run, i) => {
-              const startedAt = Date.parse(run.started_at!) 
-              const lastModified = Date.parse(run.last_modified!)
-              const duration = `${Math.floor((lastModified - startedAt)/1000/60)}m ${Math.floor(((lastModified - startedAt)/1000)%60)}s` 
+              const startedAt = Date.parse(run.started_at!);
+              const lastModified = Date.parse(run.last_modified!);
+              const duration = `${Math.floor(
+                (lastModified - startedAt) / 1000 / 60
+              )}m ${Math.floor(((lastModified - startedAt) / 1000) % 60)}s`;
               return (
                 <Accordion defaultExpanded={i === 0 ? true : undefined}>
                   <AccordionSummary
@@ -53,7 +61,12 @@ const PipelineRuns: React.FC<PipelineRunsProps> = ({ groupId, pipelineId }) => {
                     <b>{run.name}</b>
                   </AccordionSummary>
                   <AccordionDetails>
-                    <Table dark bordered style={{ margin: 0}} className={styles["runs-table"]}>
+                    <Table
+                      dark
+                      bordered
+                      style={{ margin: 0 }}
+                      className={styles['runs-table']}
+                    >
                       <thead>
                         <tr>
                           <th>#</th>
@@ -73,15 +86,20 @@ const PipelineRuns: React.FC<PipelineRunsProps> = ({ groupId, pipelineId }) => {
                         <td>{run.last_modified}</td>
                       </tr>
                     </Table>
-                    <pre className={styles["logs"]}>
-                      {run.logs}
-                    </pre>
+                    <pre className={styles['logs']}>{run.logs}</pre>
                   </AccordionDetails>
                   <AccordionActions>
-                    <Button variant="outlined" onClick={() => {history.push(`${pathname}/${run.uuid}`)}}>View Task Executions</Button>
+                    <Button
+                      variant="outlined"
+                      onClick={() => {
+                        history.push(`${pathname}/${run.uuid}`);
+                      }}
+                    >
+                      View Task Executions
+                    </Button>
                   </AccordionActions>
                 </Accordion>
-              )
+              );
             })
           ) : (
             <SectionMessage type="info">
