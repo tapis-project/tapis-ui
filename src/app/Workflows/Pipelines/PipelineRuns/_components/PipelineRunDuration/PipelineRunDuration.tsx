68867fcbd2b4623390a5e821601516afbc5e2fdb
Nathan Freeman <nathan.dean.freeman@gmail.com>
2024-08-15 14:28:47 -0500

    Add counter functionality with duration

--- src/app/Workflows/Pipelines/PipelineRuns/_components/PipelineRunDuration/PipelineRunDuration.tsx
+++ src/app/Workflows/Pipelines/PipelineRuns/_components/PipelineRunDuration/PipelineRunDuration.tsx
@@ -1,16 +1,50 @@
 import { Workflows } from '@tapis/tapis-typescript';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PipelineRunDuration: React.FC<{
   run: Workflows.PipelineRun;
   style?: React.CSSProperties;
 }> = ({ run, style }) => {
+  const isTerminal = (status: Workflows.EnumRunStatus) => {
+    if (
+      [
+        Workflows.EnumRunStatus.Failed,
+        Workflows.EnumRunStatus.Completed,
+        Workflows.EnumRunStatus.Terminated,
+        Workflows.EnumRunStatus.Suspended
+      ].includes(status)
+    ) {
+      return true
+    }
+
+    return false
+  }
   const startedAt = Date.parse(run.started_at!);
-  const lastModified = Date.parse(run.last_modified!);
-  const duration = `${Math.floor(
-    (lastModified - startedAt) / 1000 / 60
-  )}m ${Math.floor(((lastModified - startedAt) / 1000) % 60)}s`;
-  return <div style={style}>{duration}</div>;
+  const now = Date.parse(run.last_modified!);
+  let initialTime = [
+    Math.floor((now - startedAt) / 1000 / 60),
+    Math.floor(((now - startedAt) / 1000) % 60)
+  ]
+  const [time, setTime] = useState(initialTime)
+
+  useEffect(() => {
+    if (run.status && isTerminal(run.status)) {
+      return
+    }
+    const interval = setInterval(() => {
+      let newTime
+      if (time[1] === 59) {
+        newTime = [time[0] + 1, 0]
+      } else {
+        newTime = [time[0], time[1] + 1]
+      }
+      setTime(newTime)
+    }, 1000)
+
+    return () => clearInterval(interval);
+  }, [time])
+
+  return <div style={style}>{time[0]}m {time[1]}s</div>;
 };
 
 export default PipelineRunDuration;
