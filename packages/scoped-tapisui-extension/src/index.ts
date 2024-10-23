7020bbe906dc28e0c763d7b9ed57d2907930ef18
Yizhou Wang <yizhouwang@utexas.edu>
2024-10-23 16:07:15 -0500

    style

--- packages/scoped-tapisui-extension/src/index.ts
+++ packages/scoped-tapisui-extension/src/index.ts
@@ -29,12 +29,14 @@ const extension = createExtension({
   authMethods: ['implicit', 'password'],
   logo: {
     // filePath from https://x.com/SEIS_SCOPED
-    filePath: 'https://pbs.twimg.com/profile_banners/1431832137997582336/1631311944/1500x500',
+    filePath:
+      'https://pbs.twimg.com/profile_banners/1431832137997582336/1631311944/1500x500',
     text: 'SCOPED',
   },
   icon: {
     // filePath from https://www.youtube.com/@scoped6259
-    filePath: 'https://yt3.googleusercontent.com/ZW2nLowlwtI10OWxxGp0cwV-20_djQdrNBzF7rU_7_a4EfBzDKMHx_GPlhEqw_mPMrCQWPVCSg=s160-c-k-c0x00ffffff-no-rj',
+    filePath:
+      'https://yt3.googleusercontent.com/ZW2nLowlwtI10OWxxGp0cwV-20_djQdrNBzF7rU_7_a4EfBzDKMHx_GPlhEqw_mPMrCQWPVCSg=s160-c-k-c0x00ffffff-no-rj',
     text: 'SCOPED',
   },
 });
