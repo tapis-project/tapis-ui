10bdbb7545879f0e68f28685992c7650d59a509a
alex fields <liftandthink@gmail.com>
2024-11-06 11:46:24 -0600

    create apps and files help.

---
+++ src/app/_components/Help/FilesHelp/FilesHelp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Help } from '@tapis/tapisui-common';
+
+const FilesHelp: React.FC = () => {
+    return (
+    <Help
+    title = "Tapis Files"
+    iframeUrl='https://tapis.readthedocs.io/en/latest/technical/files.html'
+    />
+    )
+}
