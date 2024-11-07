948a45312fe043d78d26c916c96c3bb0e961f398
Nathan Freeman <nathan.dean.freeman@gmail.com>
2024-11-07 15:50:01 -0600

    New icons for Pipeline run statuses

--- src/app/Workflows/Pipelines/Pipeline/_components/DagView/DagViewHeader/DagViewHeader.tsx
+++ src/app/Workflows/Pipelines/Pipeline/_components/DagView/DagViewHeader/DagViewHeader.tsx
@@ -67,12 +67,6 @@ const DagViewHeader: React.FC<DagViewHeaderProps> = ({
         <div>
           <PipelineRunSummary status={run.status}>
             {pipelineId}
-            <Link
-              style={{ color: '#707070' }}
-              to={`/workflows/pipelines/${groupId}/${pipelineId}/runs/${pipelineRunUuid}`}
-            >
-              {run.name || '?'}
-            </Link>
             {![
               Workflows.EnumRunStatus.Completed,
               Workflows.EnumRunStatus.Failed,
@@ -102,6 +96,12 @@ const DagViewHeader: React.FC<DagViewHeaderProps> = ({
                 Terminate
               </Button>
             )}
+            <Link
+              style={{ color: '#707070' }}
+              to={`/workflows/pipelines/${groupId}/${pipelineId}/runs/${pipelineRunUuid}`}
+            >
+              {run.name || '?'}
+            </Link>
             <PipelineRunDuration style={{ fontSize: '12px' }} run={run} />
             <Button
               size="small"
