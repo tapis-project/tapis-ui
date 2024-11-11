becdfa9f4cc35b866ae0301f98c6c40c414862a8
carrythemountain <liftandthink@gmail.com>
2024-11-11 11:45:26 -0600

    Task/t UI 411 mixed page headers (#432)
    * create apps and files help.
    
    * add indexes
    
    * work on headers
    
    * fix headers
    
    * Make apps (create new app) and systems (create new system) buttons uniform
    
    * create layout for jobs, update layout-module for jobs and files.
    
    * npm run prettier
    
    * Remove commented code, fix apps and systems toolbar to reflect create system, create app, accordingly.

---
+++ src/app/_components/Help/AppsHelp/AppsHelp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Help } from '@tapis/tapisui-common';
+
+const AppsHelp: React.FC = () => {
+  return (
+    <Help
+      title="Tapis Apps"
+      iframeUrl="https://tapis.readthedocs.io/en/latest/technical/apps.html"
+    />
+  );
+};
+
+export default AppsHelp;
