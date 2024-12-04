7ee62aee4053dce8a14b9713c7dfaa824037ec44
alex fields <liftandthink@gmail.com>
2024-12-03 18:40:04 -0600

    fix value attributes

--- src/app/Workflows/_components/Toolbar/CreateArchiveModal/CreateArchiveModal.tsx
+++ src/app/Workflows/_components/Toolbar/CreateArchiveModal/CreateArchiveModal.tsx
@@ -150,20 +150,21 @@ const TapisSystemArchiveForm: React.FC<FormProps> = ({ onSubmit }) => {
             description={``}
             aria-label="Input"
           />
-          <FormikSelect
-            name="system_id"
-            label={'Tapis system'}
-            required={true}
-            description={'A Tapis system'}
-          >
-            <option disabled selected>
-              -- select a system --
-            </option>
-            ;
-            {Object.values(systems).map((system) => {
-              return <option value={system.id}>{system.id}</option>;
-            })}
-          </FormikSelect>
+            <FormikSelect
+              name="system_id"
+              label={'Tapis system'}
+              required={true}
+              description={'A Tapis system'}
+            >
+              <option value="" disabled selected>
+                -- select a system --
+              </option>
+              {Object.values(systems).map((system) => (
+                <option value={system.id}>
+                  {system.id}
+                </option>
+              ))}
+            </FormikSelect>
           <FormikInput
             name="archive_dir"
             label="Archive directory"
