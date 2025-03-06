7cfc4b9f636506918e452098cb597b0e74a42a74
alex fields <liftandthink@gmail.com>
2025-03-06 11:10:51 -0600

    fix error decoder

--- packages/tapisui-api/src/utils/errorDecoder.test.ts
+++ packages/tapisui-api/src/utils/errorDecoder.test.ts
@@ -24,13 +24,13 @@ describe('Error Decoder', () => {
   it('Returns a json error', () => {
     // The json method returns a Promise
     const jsonError = { 
-      json: () => Promise.resolve({ message: 'JSON error' })
+      json: () => Promise.resolve({ message: 'An unexpected error occurred' })
     };
 
     const promise = errorDecoder<ResultType>(
       () => new Promise((_, reject) => reject(jsonError))
     );
     
-    return expect(promise).rejects.toThrow('JSON error');
+    return expect(promise).rejects.toThrow('An unexpected error occurred');
   });
 });
