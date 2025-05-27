465d7270777b75642fbe11728aba17d09f3b37b6
Nathan Freeman <nathan.dean.freeman@gmail.com>
2025-05-27 17:53:42 -0500

    add nice submitted time

--- packages/icicle-tapisui-extension/src/pages/MLEdge/Analysis/AnalysisForm.tsx
+++ packages/icicle-tapisui-extension/src/pages/MLEdge/Analysis/AnalysisForm.tsx
@@ -188,6 +188,20 @@ const datasets = [
   },
 ];
 
+const secondsToDhms = (seconds: number) => {
+  var d = Math.floor(seconds / (3600 * 24));
+  var h = Math.floor((seconds % (3600 * 24)) / 3600);
+  var m = Math.floor((seconds % 3600) / 60);
+  var s = Math.floor(seconds % 60);
+
+  var dDisplay = d > 0 ? d + 'd ' : '';
+  var hDisplay = h > 0 ? h + 'h ' : '';
+  var mDisplay = m > 0 ? m + 'm ' : '';
+  var sDisplay = s > 0 ? s + 's ' : '';
+
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+};
+
 const AnalysisForm: React.FC = () => {
   const [analyses, setAnalyses] = useState<Analysis[]>([initialValues]);
   const [recentAnalyses, setRecentAnalyses] = useState<any[]>([]);
@@ -891,7 +905,7 @@ const AnalysisForm: React.FC = () => {
 
               return {
                 id: job.uuid!,
-                age: `${age}s ago`,
+                age: `${secondsToDhms(age)} ago`,
                 startedAt: job.created!,
                 endedAt: job.ended!,
                 status: job.status!,
@@ -916,7 +930,7 @@ const AnalysisForm: React.FC = () => {
               {
                 field: 'age',
                 headerName: 'Submitted',
-                width: 100,
+                width: 150,
               },
               {
                 field: 'status',
