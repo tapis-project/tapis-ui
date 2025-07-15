2723419e4488b9f47b333bdaebfa2fce52b1a7af
alex fields <liftandthink@gmail.com>
2025-07-15 16:14:56 -0500

    create updateClient api. work on updateClient hook.

--- packages/tapisui-api/src/authenticator/updateClient.ts
+++ packages/tapisui-api/src/authenticator/updateClient.ts
@@ -1,2 +1,28 @@
 import { Authenticator } from '@tapis/tapis-typescript';
 import { errorDecoder, apiGenerator } from '../utils';
+
+const updateClient = (
+  clientId: string,
+  basePath: string,
+  jwt: string,
+  callback_url: string,
+  display_name: string
+) => {
+  const api: Authenticator.ClientsApi = apiGenerator<Authenticator.ClientsApi>(
+    Authenticator,
+    Authenticator.ClientsApi,
+    basePath,
+    jwt
+  );
+
+  const requestParameters: Authenticator.UpdateClientRequest = {
+    clientId,
+    updateClient: { callback_url, display_name },
+  };
+
+  return errorDecoder<Authenticator.RespUpdateClient>(() =>
+    api.updateClient(requestParameters)
+  );
+};
+
+export default updateClient;
