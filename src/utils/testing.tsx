cbc1357da52ba211f01c384a737c95ca9d42d4b5
Mingyu-Wei <2370686225@qq.com>
2025-10-15 01:45:11 -0500

    fix bugs for testing

--- src/utils/testing.tsx
+++ src/utils/testing.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import { TapisProvider } from '@tapis/tapisui-hooks';
+import React from "react";
+import { BrowserRouter, Router } from "react-router-dom";
+import { render } from "@testing-library/react";
+import { TapisProvider } from "@tapis/tapisui-hooks";
 export default function renderComponent(component: any, history: any = null) {
   if (history) {
     return render(
-      <TapisProvider basePath="tapis.test">
+      <TapisProvider basePath="tapis.test" mlHubBasePath="mlhub.test">
         <Router history={history}>{component}</Router>
       </TapisProvider>
     );
   }
   return render(
-    <TapisProvider basePath="tapis.test">
+    <TapisProvider basePath="tapis.test" mlHubBasePath="mlhub.test">
       <BrowserRouter>{component}</BrowserRouter>
     </TapisProvider>
   );
