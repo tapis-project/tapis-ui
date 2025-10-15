6a0719e10614cfc5a3f66848df1f8838650a8d05
Mingyu-Wei <2370686225@qq.com>
2025-10-14 23:33:05 -0500

    ML-Hub: add resolveMLHubBasePath + wire basePath in provider/context

--- src/utils/resolveMLHubBasePath.ts
+++ src/utils/resolveMLHubBasePath.ts
@@ -1,13 +1,8 @@
 export const resolveMLHubBasePath = () => {
-  if (import.meta.env.DEV) {
-    // local dev: use proxy to avoid CORS
-    return '/v3';
-  }
-
-  const basePath = 'https://dev.develop.tapis.io';
+  const basePath = "https://dev.develop.tapis.io";
   if (!basePath) {
-    throw new Error('VITE_MLHUB_BASE_URL environment variable is not set');
+    throw new Error("VITE_MLHUB_BASE_URL environment variable is not set");
   }
 
-  return basePath.endsWith('/v3') ? basePath : basePath + '/v3';
+  return basePath.endsWith("/v3") ? basePath : basePath + "/v3";
 };
