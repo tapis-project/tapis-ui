5d522b989c8a142ed0d2ecce059d5b1c5db8bfb0
Mingyu-Wei <2370686225@qq.com>
2025-10-21 21:06:49 -0500

    set proxy to deal with CORS

--- src/utils/resolveMLHubBasePath.ts
+++ src/utils/resolveMLHubBasePath.ts
@@ -1,5 +1,14 @@
 export const resolveMLHubBasePath = () => {
+<<<<<<< HEAD
   const basePath = "https://dev.develop.tapis.io";
+=======
+  if (import.meta.env.DEV) {
+    // local dev: use proxy to avoid CORS
+    return "/v3";
+  }
+
+  const basePath = import.meta.env.VITE_MLHUB_BASE_URL;
+>>>>>>> cd401f06 (set proxy to deal with CORS)
   if (!basePath) {
     throw new Error("VITE_MLHUB_BASE_URL environment variable is not set");
   }
