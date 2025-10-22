8cbcb1bf17ae750510e2c47be3060aa55a41d0f2
Mingyu-Wei <2370686225@qq.com>
2025-10-22 10:15:09 -0500

    hardcode mlhubBasePath

--- src/utils/resolveMLHubBasePath.ts
+++ src/utils/resolveMLHubBasePath.ts
@@ -4,7 +4,7 @@ export const resolveMLHubBasePath = () => {
     return '/v3';
   }
 
-  const basePath = import.meta.env.VITE_MLHUB_BASE_URL;
+  const basePath = "https://dev.develop.tapis.io";
   if (!basePath) {
     throw new Error('VITE_MLHUB_BASE_URL environment variable is not set');
   }
