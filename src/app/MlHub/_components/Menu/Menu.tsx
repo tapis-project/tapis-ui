fced6834f32d683b4926b2bd05c491ac97951b02
Mingyu-Wei <2370686225@qq.com>
2025-10-28 03:23:05 -0500

    Template chatbot

--- src/app/MlHub/_components/Menu/Menu.tsx
+++ src/app/MlHub/_components/Menu/Menu.tsx
@@ -1,48 +1,59 @@
-import React from 'react';
-import { Navbar, Nav, NavItem, Collapse, Button } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import { Icon } from '@tapis/tapisui-common';
-import styles from './Menu.module.scss';
+import React from "react";
+import { Navbar, Nav, NavItem, Collapse, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import { Icon } from "@tapis/tapisui-common";
+import styles from "./Menu.module.scss";
 
-const Menu: React.FC = () => {
+type MenuProps = {
+  onToggleChat?: () => void;
+};
+
+const Menu: React.FC<MenuProps> = ({ onToggleChat }) => {
   return (
     <Navbar color="light" light expand={true}>
       <Collapse isOpen={true} navbar>
         <Nav className="me-auto" navbar>
-          <NavItem className={styles['nav-item']}>
+          <NavItem className={styles["nav-item"]}>
             <Link to="/ml-hub">
               <Button>
                 <Icon name="dashboard"></Icon> Dashboard
               </Button>
             </Link>
           </NavItem>
-          <NavItem className={styles['nav-item']}>
+          <NavItem className={styles["nav-item"]}>
             <Link to="/ml-hub/models">
               <Button>
                 <Icon name="simulation"></Icon> Models
               </Button>
             </Link>
           </NavItem>
-          <NavItem className={styles['nav-item']}>
+          <NavItem className={styles["nav-item"]}>
             <Link to="/ml-hub/datasets">
               <Button>
                 <Icon name="search-folder"></Icon> Datasets
               </Button>
             </Link>
           </NavItem>
-          <NavItem className={styles['nav-item']}>
+          <NavItem className={styles["nav-item"]}>
             <Link to="/ml-hub/inference">
               <Button>
                 <Icon name="multiple-coversation"></Icon> Inference
               </Button>
             </Link>
           </NavItem>
-          <NavItem className={styles['nav-item']}>
+          <NavItem className={styles["nav-item"]}>
             <Link to="/ml-hub/training">
               <Button>
                 <Icon name="data-processing"></Icon> Training
               </Button>
             </Link>
+          </NavItem>
+        </Nav>
+        <Nav navbar>
+          <NavItem className={styles["nav-item"]}>
+            <Button onClick={onToggleChat}>
+              <Icon name="multiple-coversation"></Icon> Model Chat
+            </Button>
           </NavItem>
         </Nav>
       </Collapse>
