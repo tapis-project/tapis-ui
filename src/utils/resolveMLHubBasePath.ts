7be4fc24e7877019a73282a68a32cf0fe9e7dd50
Mingyu-Wei <2370686225@qq.com>
2025-10-27 23:57:21 -0500

    run prettier

--- src/utils/resolveMLHubBasePath.ts
+++ src/utils/resolveMLHubBasePath.ts
@@ -1,9 +1,9 @@
 export const resolveMLHubBasePath = () => {
   // TODO: remove this once we have a proper environment variable
-  const basePath = "https://dev.develop.tapis.io";
+  const basePath = 'https://dev.develop.tapis.io';
   if (!basePath) {
-    throw new Error("VITE_MLHUB_BASE_URL environment variable is not set");
+    throw new Error('VITE_MLHUB_BASE_URL environment variable is not set');
   }
 
-  return basePath.endsWith("/v3") ? basePath : basePath + "/v3";
+  return basePath.endsWith('/v3') ? basePath : basePath + '/v3';
 };
