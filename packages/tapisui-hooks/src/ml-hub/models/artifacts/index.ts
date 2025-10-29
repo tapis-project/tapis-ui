2be9ee78766b1afe818cf8a9001bd824642fe7ac
Mingyu-Wei <2370686225@qq.com>
2025-10-29 01:32:43 -0500

    add api for add metadata

--- packages/tapisui-hooks/src/ml-hub/models/artifacts/index.ts
+++ packages/tapisui-hooks/src/ml-hub/models/artifacts/index.ts
@@ -1,2 +1,3 @@
 export { default as useListModelArtifacts } from './useListModelArtifacts';
 export { default as useGetModelArtifact } from './useGetModelArtifact';
+export { default as useCreateModelMetadata } from './useCreateModelMetadata';
