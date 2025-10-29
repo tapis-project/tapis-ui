b3b72e6f782b77795df8b48ae4353dc1c9f6739c
Mingyu-Wei <2370686225@qq.com>
2025-10-28 21:02:22 -0500

    Add list_model_ingestions

--- packages/tapisui-api/src/ml-hub/models/ingestions/index.ts
+++ packages/tapisui-api/src/ml-hub/models/ingestions/index.ts
@@ -1,2 +1,3 @@
 export { default as getModelIngestion } from './getModelIngestion';
 export { default as ingestModelByPlatform } from './ingestModelByPlatform';
+export { default as listModelIngestions } from './listModelIngestions';
