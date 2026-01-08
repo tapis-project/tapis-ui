8bcc450c5dbdf54c783fce7b9ed8cb88e03dfafd
Nathan Freeman <nathan.dean.freeman@gmail.com>
2026-01-08 17:00:59 -0600

    update the mlhub baseurl

--- src/utils/resolveMLHubBasePath.ts
+++ src/utils/resolveMLHubBasePath.ts
@@ -1,7 +1,7 @@
 export const resolveMLHubBasePath = () => {
   // TODO: remove this once we have a proper environment variable
-  // const basePath = 'https://tacc.tapis.io/v3/mlhub';
-  const basePath = 'http://localhost:64722';
+  const basePath = 'https://tacc.tapis.io/v3/mlhub';
+  // const basePath = 'http://localhost:64722';
   return basePath;
   if (!basePath) {
     throw new Error('VITE_MLHUB_BASE_URL environment variable is not set');
