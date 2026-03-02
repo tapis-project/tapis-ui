0859cfc959621962fe6e15787c16a9180ccedd2a
Nathan Freeman <nathan.dean.freeman@gmail.com>
2026-03-02 14:23:34 -0600

    Add v3/workflows to mlhub base url

--- src/utils/resolveMLHubBasePath.ts
+++ src/utils/resolveMLHubBasePath.ts
@@ -3,7 +3,7 @@ import { resolveBasePath } from './resolveBasePath';
 export const resolveMLHubBasePath = () => {
   let basePath = import.meta.env.VITE_MLHUB_BASE_URL;
   if (!basePath) {
-    basePath = resolveBasePath();
+    basePath = resolveBasePath() + "/v3/mlhub";
   }
   return basePath;
 };
