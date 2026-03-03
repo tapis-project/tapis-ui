984d730c0970ac5290b3b29bb32d3e1f1977085d
Amit Vyas <23amitvyas@gmail.com>
2026-03-03 18:08:10 -0500

    Add Food Systems portal pages to Icicle extension (#510)

--- packages/icicle-tapisui-extension/src/pages/FoodFlowPortal/FoodFlowPortal.tsx
+++ packages/icicle-tapisui-extension/src/pages/FoodFlowPortal/FoodFlowPortal.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { SectionHeader } from '@tapis/tapisui-common';
 import { Component } from '@tapis/tapisui-extensions-core';
 
-export const TrainingCatalog: Component = ({ accessToken }) => {
+export const FoodFlowPortal: Component = ({ accessToken }) => {
   return (
     <div
       style={{
@@ -16,7 +16,8 @@ export const TrainingCatalog: Component = ({ accessToken }) => {
       {accessToken ? (
         <iframe
           style={{ flexGrow: 1, border: 'none' }}
-          src={`https://icicle-ai.github.io/training-catalog/docs/intro`}
+          src="https://gnnfoodflowportal.pods.icicleai.tapis.io/"
+          title="Food Flow Portal"
         />
       ) : (
         <>Invalid JWT. Log out of TapisUI then log back in</>
@@ -25,4 +26,4 @@ export const TrainingCatalog: Component = ({ accessToken }) => {
   );
 };
 
-export default TrainingCatalog;
+export default FoodFlowPortal;
