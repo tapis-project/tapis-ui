50c53ccf52ff4508901b41a53b592f8135c9fbff
HARIKESH BYRANDURGA GOPINATH <35234606+harikeshbg@users.noreply.github.com>
2026-07-10 18:12:24 -0400

    Embedded Intelligent Edge Management Services on ICICLEAI Tapis UI. (#544)

--- packages/icicle-tapisui-extension/src/pages/DomainAgnosticCI/DomainAgnosticCI.tsx
+++ packages/icicle-tapisui-extension/src/pages/DomainAgnosticCI/DomainAgnosticCI.tsx
@@ -65,6 +65,13 @@ const services: ServiceItem[] = [
     name: 'HARP',
     link: 'https://github.com/ICICLE-ai/harp',
   },
+  {
+    label:
+      'Intelligent Edge Management Service : Intelligent Orchestration and Controller Service for deploying and managing AI applications on edge devices.',
+    name: 'Intelligent Edge Management Service : Edge Orchestration Platform',
+    link: '/intelligent-edge-management-service',
+    internal: true,
+  },
 ];
 
 const sortedServices = sortServices(services);
